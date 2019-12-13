95503d7e1c95a3874a072af093d2c88e27b7e22f
zhaotianxin <ztxemail@163.com>
2019-12-13 16:38:37 +0800

    [IMP] 调用notify-service 获取项目层的发送设置

---
+++ src/main/java/io/choerodon/agile/infra/dto/TargetUserDTO.java
@@ -0,0 +1,59 @@
+package io.choerodon.agile.infra.dto;
+
+
+import io.choerodon.mybatis.entity.BaseDTO;
+import io.swagger.annotations.ApiModelProperty;
+
+public class TargetUserDTO extends BaseDTO {
+
+    private Long id;
+    @ApiModelProperty("接收消息对象的类型")
+    private String type;
+    @ApiModelProperty("接收消息对象的id")
+    private Long userId;
+    @ApiModelProperty("平台设置表的id")
+    private Long messageSettingId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getMessageSettingId() {
+        return messageSettingId;
+    }
+
+    public void setMessageSettingId(Long messageSettingId) {
+        this.messageSettingId = messageSettingId;
+    }
+
+    @Override
+    public String toString() {
+        return "TargetUserDTO{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", userId='" + userId + '\'' +
+                ", messageSettingId=" + messageSettingId +
+                ", objectVersionNumber=" + super.getObjectVersionNumber() +
+                '}';
+    }
+}
--- src/main/java/io/choerodon/agile/infra/feign/NotifyFeignClient.java
+++ src/main/java/io/choerodon/agile/infra/feign/NotifyFeignClient.java
@@ -1,11 +1,12 @@
 package io.choerodon.agile.infra.feign;
 
 import io.choerodon.agile.infra.feign.fallback.NotifyFeignClientFallback;
+import io.choerodon.agile.infra.feign.vo.MessageSettingVO;
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.notify.NoticeSendDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "notify-service", path = "/v1/notices", fallback = NotifyFeignClientFallback.class)
 public interface NotifyFeignClient {
@@ -20,5 +21,12 @@ public interface NotifyFeignClient {
 
     @PostMapping
     void postEmail(@RequestBody NoticeSendDTO dto);
+
+    @GetMapping("/v1/projects/{project_id}/message_settings/type/{notify_type}/code/{code}")
+    ResponseEntity<MessageSettingVO> getMessageSetting(@PathVariable(value = "project_id") Long projectId,
+                                                       @PathVariable(value = "notify_type") String notifyType,
+                                                       @PathVariable(value = "code") String code,
+                                                       @RequestParam(value = "env_id", required = false) Long envId,
+                                                       @RequestParam(value = "event_name", required = false) String eventName);
 
 }
