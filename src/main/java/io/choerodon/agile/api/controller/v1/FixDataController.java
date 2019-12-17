1db84005a02de6ddc2ab67d3d54d154b24c33e21
HuangFuqiang <fuqianghuang01@gmail.com>
2019-12-17 18:46:33 +0800

    [ADD]添加迁移数据接口

--- src/main/java/io/choerodon/agile/api/controller/v1/FixDataController.java
+++ src/main/java/io/choerodon/agile/api/controller/v1/FixDataController.java
@@ -2,11 +2,14 @@ package io.choerodon.agile.api.controller.v1;
 
 import io.choerodon.agile.app.service.*;
 import io.choerodon.agile.infra.dto.MessageDetailDTO;
+import io.choerodon.agile.infra.feign.NotifyFeignClient;
 import io.choerodon.core.annotation.Permission;
 import io.choerodon.core.enums.ResourceType;
 import io.choerodon.core.iam.InitRoleCode;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +26,16 @@ import java.util.List;
 @RequestMapping(value = "/v1/fix_data")
 public class FixDataController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FixDataController.class);
+
     @Autowired
     private FixDataService fixDataService;
 
     @Autowired
     private NoticeService noticeService;
+
+    @Autowired
+    private NotifyFeignClient notifyFeignClient;
 
 
     @Permission(type = ResourceType.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR, InitRoleCode.SITE_DEVELOPER})
@@ -47,10 +55,19 @@ public class FixDataController {
     }
 
     @Permission(type = ResourceType.PROJECT, roles = {InitRoleCode.PROJECT_OWNER})
-    @ApiOperation("迁移agile_message_detail到框架")
+    @ApiOperation("【0.20 BASE】迁移agile_message_detail到框架")
     @GetMapping("/migrate_message")
     public ResponseEntity<List<MessageDetailDTO>> migrateMessageDetail(@ApiParam(value = "项目id", required = true)
                                                                        @PathVariable(name = "project_id") Long projectId) {
         return new ResponseEntity<>(noticeService.migrateMessageDetail(projectId),HttpStatus.OK);
+    }
+
+    @Permission(type = ResourceType.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR, InitRoleCode.SITE_DEVELOPER})
+    @ApiOperation("【0.20 BASE】启动敏捷迁移到Base服务task")
+    @GetMapping("/migration_to_base")
+    public ResponseEntity migrateToBase() {
+        LOGGER.info("==============================>>>>>>>> AGILE Data Migrate Start In Controller <<<<<<<<=================================");
+        notifyFeignClient.checkLog("0.20.0", "agile");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
