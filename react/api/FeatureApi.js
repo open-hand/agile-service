52d281032ce62660efce7f667db71f6bc2605558
laincarl <1047972853@qq.com>
2020-02-03 14:13:34 +0800

    [FIX]更改接口前缀

--- react/api/FeatureApi.js
+++ react/api/FeatureApi.js
@@ -3,17 +3,17 @@ import { getProjectId, getOrganizationId } from '../common/utils';
 
 export function getFeatures(pagination, searchVO, sort) {
   const { size, page } = pagination;
-  return axios.post(`/agile-pro/v1/projects/${getProjectId()}/issues/program?size=${size}&page=${page}&organizationId=${getOrganizationId()}`, searchVO, {
+  return axios.post(`/agile/v1/projects/${getProjectId()}/issues/program?size=${size}&page=${page}&organizationId=${getOrganizationId()}`, searchVO, {
     params: sort,
   });
 }
 export function getFeaturesInProject() {
-  return axios.get(`/agile-pro/v1/projects/${getProjectId()}/issues/features?organizationId=${getOrganizationId()}`);
+  return axios.get(`/agile/v1/projects/${getProjectId()}/issues/features?organizationId=${getOrganizationId()}`);
 }
 
 export function getFeaturesByEpic(epicId) {
-  return axios.get(`/agile-pro/v1/projects/${getProjectId()}/issues/feature/select_data?organizationId=${getOrganizationId()}${epicId ? `&epicId=${epicId}` : ''}`);
+  return axios.get(`/agile/v1/projects/${getProjectId()}/issues/feature/select_data?organizationId=${getOrganizationId()}${epicId ? `&epicId=${epicId}` : ''}`);
 }
 export function exportFeatures(search) {
-  return axios.post(`/zuul/agile-pro/v1/projects/${getProjectId()}/issues/program/export?organizationId=${getOrganizationId()}`, search, { responseType: 'arraybuffer' });
+  return axios.post(`/zuul/agile/v1/projects/${getProjectId()}/issues/program/export?organizationId=${getOrganizationId()}`, search, { responseType: 'arraybuffer' });
 }
