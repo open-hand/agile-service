a472a38a54494edf2775e12cce11061cd1259681
laincarl <1047972853@qq.com>
2020-04-21 14:30:19 +0800

    [IMP]优化值空判断和用户bug

--- react/routes/Issue/components/search/custom-fields/field/MemberField.js
+++ react/routes/Issue/components/search/custom-fields/field/MemberField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Select } from 'choerodon-ui';
 import { unionBy } from 'lodash';
@@ -7,9 +7,9 @@ import SelectFocusLoad from '@/components/SelectFocusLoad';
 
 let users = [];
 const { Option } = Select;
-// TODO: 已保存筛选条件含有用户，并且这个时候select并没有显示，那么选了自定义筛选，用户那显示为空
 function MemberField({ field, value, onChange }) {
-  const { name } = field;
+  const { code, name } = field;
+  const [, setValue] = useState(0);
   return (
     <SelectFocusLoad
       {...configTheme({
@@ -26,14 +26,21 @@ function MemberField({ field, value, onChange }) {
       showCheckAll={false}
       allowClear
       placeholder={name}
-      saveList={(v) => { users = unionBy(users, v, 'id'); }}
+      saveList={(v) => {
+        const shouldRender = users.length === 0 && value && value.length > 0;
+        users = unionBy(users, v, 'id'); 
+        // 已保存筛选条件含有用户，并且这个时候select并没有显示，那么选了自定义筛选，要渲染一次
+        if (users.length > 0 && shouldRender) {
+          setValue(Math.random());
+        }
+      }}
       filter
       onChange={onChange}
       value={value}
       getPopupContainer={triggerNode => triggerNode.parentNode}
       render={user => <Option value={user.id}>{user.realName || user.loginName}</Option>}
     >
-      {/* <Option value="0">未分配</Option> */}
+      {code === 'assigneeId' ? <Option value="0">未分配</Option> : undefined}
     </SelectFocusLoad>
   );
 }
