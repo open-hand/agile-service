150497894e5a658cbed9f1019223f4df8bcbad49
laincarl <1047972853@qq.com>
2020-04-22 10:29:18 +0800

    [FIX]增加筛选项

---
+++ react/routes/Issue/components/search/custom-fields/field/PriorityField.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { unionBy } from 'lodash';
+import SelectFocusLoad from '@/components/SelectFocusLoad';
+import { configTheme } from '@/common/utils';
+
+let list = [];
+function PriorityField({ field, value, onChange }) {
+  const [, setValue] = useState(0);
+  return (
+    <SelectFocusLoad
+      {...configTheme({
+        list,
+        textField: 'name',
+        valueFiled: 'id',
+        parseNumber: true,
+      })}
+      type="priority"
+      loadWhenMount
+      style={{ width: 120, margin: 0 }}
+      mode="multiple"
+      showCheckAll={false}
+      allowClear
+      dropdownMatchSelectWidth={false}
+      placeholder={field.name}
+      saveList={(v) => {
+        const shouldRender = list.length === 0 && value && value.length > 0;
+        list = unionBy(list, v, 'id'); 
+        // 已保存筛选条件含有用户，并且这个时候select并没有显示，那么选了自定义筛选，要渲染一次
+        if (list.length > 0 && shouldRender) {
+          setValue(Math.random());
+        }
+      }}
+      filter
+      onChange={onChange}
+      value={value}
+      getPopupContainer={triggerNode => triggerNode.parentNode}    
+      requestArgs={[]}
+    />
+  );
+}
+export default observer(PriorityField);
