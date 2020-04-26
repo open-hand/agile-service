7280c3a34348faa7594c6e0f97f316defca3d4e6
zhaotianxin <ztxemail@163.com>
2020-04-26 11:32:43 +0800

    [FIX] 迭代计划排序不生效

--- src/main/java/io/choerodon/agile/app/service/impl/BoardServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/BoardServiceImpl.java
@@ -227,6 +227,7 @@ public class BoardServiceImpl implements BoardService {
             subStatusDTOS = fillStatusData(subStatusDTOS, statusMap);
             getDatas(subStatusDTOS, parentIds, assigneeIds, issueIds, epicIds, organizationId, parentWithSubss, issueTypeDTOMap);
             Collections.sort(subStatusDTOS, (o1, o2) -> o1.getPosition() - o2.getPosition());
+            column.setSubStatusDTOS(subStatusDTOS);
         }
         //选择故事泳道选择仅我的任务后，子任务经办人为自己，父任务经办人不为自己的情况
         if (condition) {
