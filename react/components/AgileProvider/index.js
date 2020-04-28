4a13cf56ad8872a6feb26ebc36d043c6104ed5f4
laincarl <1047972853@qq.com>
2020-04-28 17:14:44 +0800

    [FIX]修复某些接口不应该在组织层调用

--- react/components/AgileProvider/index.js
+++ react/components/AgileProvider/index.js
@@ -1,10 +1,12 @@
 import React, {
-  useContext, useRef, useEffect, useState, 
+  useContext, useRef, useEffect, useState,
 } from 'react';
+import { stores } from '@choerodon/boot';
 import { IssueTypeContext } from '@/hooks/useIssueTypes';
 import { DefaultPriorityContext } from '@/hooks/useDefaultPriority';
 import { PriorityContext } from '@/hooks/usePriorities';
 
+const { AppState } = stores;
 function wrapWithContexts(contexts, values, children) {
   return contexts.reduce((last, Context, index) => (
     <Context.Provider value={values.get(index)}>
@@ -15,13 +17,15 @@ function wrapWithContexts(contexts, values, children) {
 const AgileProvider = contexts => function AgileDataProvider({ children, projectId }) {
   const dataRef = useRef(new Map());
   contexts.forEach((context, index) => {
-    const { data: initData, refresh } = useContext(context);    
+    const { data: initData, refresh } = useContext(context);
     const [data, setData] = useState(initData);
-    
+
     const loadData = async (...args) => {
-      const res = await refresh(...args);
-      dataRef.current.set(index, { data: res, refresh: loadData });
-      setData(res);
+      if (AppState.currentMenuType.type === 'project') {
+        const res = await refresh(...args);
+        dataRef.current.set(index, { data: res, refresh: loadData });
+        setData(res);
+      }
     };
     dataRef.current.set(context, { data, refresh: loadData });
     useEffect(() => {
