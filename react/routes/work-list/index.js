c2ef706fda9a45dbe8d74e3c08ffb51a233ffb01
812913 <lwfvirgo@163.com>
2020-05-29 15:02:00 +0800

    [IMP] 更改TabKey

--- react/routes/work-list/index.js
+++ react/routes/work-list/index.js
@@ -10,9 +10,9 @@ const WorkList = ({ match }) => (
   <PageWrap
     noHeader={[]}
   >
-    <PageTab title="待办事项" route="/agile/work-list/backlog" tabKey="choerodon.code.cooperate.workList.backlog" component={withRouter(Backlog)} />
-    <PageTab title="所有问题" route="/agile/work-list/issue" tabKey="choerodon.code.cooperate.workList.issue" component={withRouter(Issue)} />
-    <PageTab title="版本列表" route="/agile/work-list/version" tabKey="choerodon.code.cooperate.workList.version" component={withRouter(Release)} />
+    <PageTab title="待办事项" route="/agile/work-list/backlog" tabKey="choerodon.code.project.cooperation.work-list.ps.backlog" component={withRouter(Backlog)} />
+    <PageTab title="所有问题" route="/agile/work-list/issue" tabKey="choerodon.code.project.cooperation.work-list.ps.issue" component={withRouter(Issue)} />
+    <PageTab title="版本列表" route="/agile/work-list/version" tabKey="choerodon.code.project.cooperation.work-list.ps.version" component={withRouter(Release)} />
   </PageWrap>  
 );
 export default WorkList;
