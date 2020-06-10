4d514c63e5a8748bc95aa1abf5fe74b5e0b56a70
superlee <superleader8@gmail.com>
2020-06-10 17:02:56 +0800

    [IMP] 修改ProjectRelationshipVO

--- src/main/java/io/choerodon/agile/api/vo/ProjectRelationshipVO.java
+++ src/main/java/io/choerodon/agile/api/vo/ProjectRelationshipVO.java
@@ -4,6 +4,7 @@ package io.choerodon.agile.api.vo;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by HuangFuqiang@choerodon.io on 2019/3/13.
@@ -11,46 +12,36 @@ import java.util.Date;
  */
 public class ProjectRelationshipVO {
 
-    @ApiModelProperty(value = "主键id")
+    @ApiModelProperty("主键Id")
     private Long id;
-
-    @ApiModelProperty(value = "项目id")
+    @ApiModelProperty("项目Id")
     private Long projectId;
-
-    @ApiModelProperty(value = "项目code")
-    private String projCode;
-
-    @ApiModelProperty(value = "项目名称")
-    private String projName;
-
-    @ApiModelProperty(value = "所属的项目群id")
+    @ApiModelProperty("项目组的项目Id")
     private Long parentId;
-
-    @ApiModelProperty(value = "项目群id")
-    private Long programId;
-
-    @ApiModelProperty(value = "开始时间")
+    @ApiModelProperty("开始时间")
     private Date startDate;
-
-    @ApiModelProperty(value = "结束时间")
+    @ApiModelProperty("结束时间")
     private Date endDate;
-
-    @ApiModelProperty(value = "是否启用")
+    @ApiModelProperty("是否启用")
     private Boolean enabled;
-
+    @ApiModelProperty("所属ProgramId")
+    private Long programId;
+    @ApiModelProperty("项目Code")
+    private String projCode;
+    @ApiModelProperty("项目Name")
+    private String projName;
+    @ApiModelProperty("项目人员总数")
+    private Integer userCount;
+    @ApiModelProperty("项目成员")
+    private List<UserVO> users;
+    @ApiModelProperty("项目群关系创建时间")
+    private Date creationDate;
+    @ApiModelProperty("负责人id")
+    private Long responsibilityUserId;
+    @ApiModelProperty("负责人")
+    private UserVO responsibilityUser;
     @ApiModelProperty(value = "版本号")
     private Long objectVersionNumber;
-
-    @ApiModelProperty(value = "人数")
-    private Integer userCount;
-
-    public Integer getUserCount() {
-        return userCount;
-    }
-
-    public void setUserCount(Integer userCount) {
-        this.userCount = userCount;
-    }
 
     public Long getId() {
         return id;
@@ -66,22 +57,6 @@ public class ProjectRelationshipVO {
 
     public void setProjectId(Long projectId) {
         this.projectId = projectId;
-    }
-
-    public String getProjCode() {
-        return projCode;
-    }
-
-    public void setProjCode(String projCode) {
-        this.projCode = projCode;
-    }
-
-    public String getProjName() {
-        return projName;
-    }
-
-    public void setProjName(String projName) {
-        this.projName = projName;
     }
 
     public Long getParentId() {
@@ -116,19 +91,75 @@ public class ProjectRelationshipVO {
         this.enabled = enabled;
     }
 
-    public Long getObjectVersionNumber() {
-        return objectVersionNumber;
-    }
-
-    public void setObjectVersionNumber(Long objectVersionNumber) {
-        this.objectVersionNumber = objectVersionNumber;
-    }
-
     public Long getProgramId() {
         return programId;
     }
 
     public void setProgramId(Long programId) {
         this.programId = programId;
+    }
+
+    public String getProjCode() {
+        return projCode;
+    }
+
+    public void setProjCode(String projCode) {
+        this.projCode = projCode;
+    }
+
+    public String getProjName() {
+        return projName;
+    }
+
+    public void setProjName(String projName) {
+        this.projName = projName;
+    }
+
+    public Integer getUserCount() {
+        return userCount;
+    }
+
+    public void setUserCount(Integer userCount) {
+        this.userCount = userCount;
+    }
+
+    public List<UserVO> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserVO> users) {
+        this.users = users;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getResponsibilityUserId() {
+        return responsibilityUserId;
+    }
+
+    public void setResponsibilityUserId(Long responsibilityUserId) {
+        this.responsibilityUserId = responsibilityUserId;
+    }
+
+    public UserVO getResponsibilityUser() {
+        return responsibilityUser;
+    }
+
+    public void setResponsibilityUser(UserVO responsibilityUser) {
+        this.responsibilityUser = responsibilityUser;
+    }
+
+    public Long getObjectVersionNumber() {
+        return objectVersionNumber;
+    }
+
+    public void setObjectVersionNumber(Long objectVersionNumber) {
+        this.objectVersionNumber = objectVersionNumber;
     }
 }
