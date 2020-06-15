0cdfadf3ec782e9c406edae63ab207b67e0a343f
812913 <lwfvirgo@163.com>
2020-06-15 14:02:21 +0800

    [IMP] 添加selectVersion，修改问题详情的修复的版本和影响的版本字段

---
+++ react/components/select/select-version/index.tsx
@@ -0,0 +1,35 @@
+import React, { useMemo, forwardRef } from 'react';
+import { Select } from 'choerodon-ui/pro';
+import { versionApi } from '@/api';
+import useSelect, { SelectConfig } from '@/hooks/useSelect';
+
+interface Props {
+  dataRef: React.RefObject<Array<any>>
+  statusArr: Array<string>
+}
+
+const SelectVersion: React.FC<Props> = forwardRef(({ dataRef, statusArr = [], ...otherProps }, ref: React.Ref<Select>) => {
+  const config = useMemo((): SelectConfig => ({
+    name: 'version',
+    textField: 'name',
+    valueField: 'name',
+    request: () => versionApi.loadNamesByStatus(statusArr),
+    middleWare: (versions) => {
+      // eslint-disable-next-line no-param-reassign
+      dataRef.current = versions;
+      return versions;
+    },
+    paging: false,
+  }), []);
+  const props = useSelect(config);
+  return (
+    <Select
+      multiple
+      ref={ref}
+      clearButton
+      {...props}
+      {...otherProps}
+    />
+  );
+});
+export default SelectVersion;
