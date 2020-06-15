6e424e75b250b72d671b8a06e4befc79d514d060
812913 <lwfvirgo@163.com>
2020-06-15 11:31:20 +0800

    [IMP] 添加Select-component

---
+++ react/components/select/select-component/index.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo, forwardRef } from 'react';
+import { Select } from 'choerodon-ui/pro';
+import { componentApi } from '@/api';
+import useSelect, { SelectConfig } from '@/hooks/useSelect';
+
+interface Props {
+  dataRef: React.RefObject<Array<any>>
+}
+
+const SelectComponent: React.FC<Props> = forwardRef(({ dataRef, ...otherProps }, ref: React.Ref<Select>) => {
+  const config = useMemo((): SelectConfig => ({
+    name: 'component',
+    textField: 'name',
+    valueField: 'name',
+    request: () => componentApi.loadAllComponents(),
+    middleWare: (components) => { 
+      const data = components.content || [];
+      // eslint-disable-next-line no-param-reassign
+      dataRef.current = data;
+      return data;
+    },
+    paging: false,
+  }), []);
+  const props = useSelect(config);
+  return (
+    <Select
+      ref={ref}
+      clearButton
+      multiple
+      combo
+      {...props}
+      {...otherProps}
+    />
+  );
+});
+export default SelectComponent;
