76ded122435a450a1d029af4c08fe072cd7014d5
laincarl <1047972853@qq.com>
2020-06-30 09:47:11 +0800

    [FIX]修复一些类型转换

--- react/stores/project/scrumBoard/ScrumBoardStore.js
+++ react/stores/project/scrumBoard/ScrumBoardStore.js
@@ -615,7 +615,7 @@ class ScrumBoardStore {
       sprintId: this.sprintId,
       rankFlag: true,
     };
-    const { id: transformId } = this.stateMachineMap[issueTypeId] ? this.stateMachineMap[issueTypeId][startStatus].find(issue => issue.endStatusId === parseInt(destinationStatus, 10)) : this.stateMachineMap[0][startStatus].find(issue => issue.endStatusId === parseInt(destinationStatus, 10));
+    const { id: transformId } = this.stateMachineMap[issueTypeId] ? this.stateMachineMap[issueTypeId][startStatus].find(issue => issue.endStatusId === destinationStatus) : this.stateMachineMap[0][startStatus].find(issue => issue.endStatusId === destinationStatus);
     return boardApi.moveIssue(issueId, transformId, data);
   };
 
@@ -797,7 +797,7 @@ class ScrumBoardStore {
     this.currentDrag = data;
   }
 
-  @action setWhichCanNotDragOn(statusId, { id: typeId }) {
+  @action setWhichCanNotDragOn(statusId, { id: typeId }) {    
     [...this.canDragOn.keys()].forEach((status) => {
       if (this.stateMachineMap[typeId]) {
         if (this.stateMachineMap[typeId][statusId].find(issue => issue.endStatusId === status)) {
