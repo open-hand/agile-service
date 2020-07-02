899f0ce44525d064b80a87e9e05abdea95c8542d
laincarl <1047972853@qq.com>
2020-07-02 14:00:49 +0800

    [IMP]选择user支持多选

--- react/components/select/select-user/index.tsx
+++ react/components/select/select-user/index.tsx
@@ -5,9 +5,10 @@ import { userApi } from '@/api';
 import useSelect, { SelectConfig } from '@/hooks/useSelect';
 import type { User } from '@/common/types';
 
+const toArray = (something: any) => (Array.isArray(something) ? something : [something]);
 interface Props {
   // 由于用户是分页的，有时候已选的用户不在第一页，这时候传id过来，会直接显示id，这里多传一个用户过来，放到options里
-  selectedUser?: User
+  selectedUser?: User | User[]
 }
 
 const SelectUser: React.FC<Props> = forwardRef(({ selectedUser, ...otherProps }, ref: React.Ref<Select>) => {
@@ -17,8 +18,14 @@ const SelectUser: React.FC<Props> = forwardRef(({ selectedUser, ...otherProps },
     valueField: 'id',
     request: ({ filter, page }) => userApi.getAllInProject(filter, page),
     middleWare: (data) => {
-      if (selectedUser && selectedUser.id && !find(data, { id: selectedUser.id })) {
-        return [selectedUser, ...data];
+      if (selectedUser) {
+        const temp: User[] = [];
+        (toArray(selectedUser).forEach((user) => {
+          if (!find(data, { id: user.id })) {
+            temp.push(user);
+          }
+        }));
+        return [...temp, ...data];
       } else {
         return data;
       }
