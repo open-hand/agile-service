926d6f98f64fa468c01a180e9921398313ca2864
laincarl <1047972853@qq.com>
2020-07-09 16:37:40 +0800

    [ADD]赋予api在调用时改变项目和组织id的能力

--- react/api/Api.ts
+++ react/api/Api.ts
@@ -37,6 +37,42 @@ class Api {
       return axios(AxiosConfig);
     }
   }
+
+  get projectId() {
+    return 0;
+  }
+
+  get orgId() {
+    return 0;
+  }
+
+  /**
+   * 目的：达到api.project(2).getList()中getList获取的projectId为2，而不是当前projectId
+   * @param Property 要覆盖的key
+   * @param value 要覆盖的值
+   */
+  overwrite(Property: string, value: any) {
+    // 以当前this为模板，创建一个
+    const temp = Object.create(this);
+    // 拷贝属性,保证两个值是相同的
+    // 设置本次特殊值getter
+    // 不直接temp[Property] = value;的原因是，如果这个属性只有getter，会报错
+    Object.defineProperty(temp, Property, {
+      get() {
+        return value;
+      },
+    });
+    
+    return temp;
+  }
+
+  project(projectId: number) {
+    return this.overwrite('projectId', projectId);
+  }
+
+  org(orgId: number) {
+    return this.overwrite('orgId', orgId);
+  }
 }
 
 export default Api;
