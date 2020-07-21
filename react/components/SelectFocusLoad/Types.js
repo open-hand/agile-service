157a8fcb03e9be0c82cb8b6fca295bac7d06ade7
812913 <lwfvirgo@163.com>
2020-07-21 11:19:10 +0800

    [IMP] 创建问题时直接分配给自己显示字符串的问题

--- react/components/SelectFocusLoad/Types.js
+++ react/components/SelectFocusLoad/Types.js
@@ -84,7 +84,7 @@ export default {
       const values = value instanceof Array ? value : [value];
       const requestQue = [];
       values.forEach((a) => {
-        if (a && typeof a === 'number' && !find(List, { id: a })) {
+        if (a && typeof a === 'string' && !find(List, { id: a })) {
           requestQue.push(userApi.getById(a));
         }
       });
