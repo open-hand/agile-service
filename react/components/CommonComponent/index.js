383ab11faa63a3eed32eb90b8ccd5c02b6de3d54
laincarl <1047972853@qq.com>
2020-08-24 10:00:56 +0800

    [DEL]删除无用文件

--- react/components/CommonComponent/index.js
+++ react/components/CommonComponent/index.js
@@ -1,5 +1,4 @@
 export UploadButton from './UploadButton';
 export UploadButtonNow from './UploadButtonNow';
 export NumericInput from './NumericInput';
-export ReadAndEdit from './ReadAndEdit';
 export DatetimeAgo from './DatetimeAgo';
