c4ef2cbff739bb345a9b014607d6e4bcb409699a
superlee <superleader8@gmail.com>
2020-08-27 10:28:14 +0800

    [ADD] 工作列表查询/更新/创建支持预计开始时间和预计结束时间

--- src/main/java/io/choerodon/agile/infra/dto/IssueConvertDTO.java
+++ src/main/java/io/choerodon/agile/infra/dto/IssueConvertDTO.java
@@ -78,6 +78,26 @@ public class IssueConvertDTO {
 
     private Boolean assigneerCondtiion;
 
+    private Date estimatedStartTime;
+
+    private Date estimatedEndTime;
+
+    public Date getEstimatedStartTime() {
+        return estimatedStartTime;
+    }
+
+    public void setEstimatedStartTime(Date estimatedStartTime) {
+        this.estimatedStartTime = estimatedStartTime;
+    }
+
+    public Date getEstimatedEndTime() {
+        return estimatedEndTime;
+    }
+
+    public void setEstimatedEndTime(Date estimatedEndTime) {
+        this.estimatedEndTime = estimatedEndTime;
+    }
+
     public Boolean getAssigneerCondtiion() {
         return assigneerCondtiion;
     }
