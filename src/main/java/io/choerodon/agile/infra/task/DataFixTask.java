3806b23a040c1636cb54658a16af15fdbadae61b
HuangFuqiang <fuqianghuang01@gmail.com>
2020-09-09 09:22:11 +0800

    [FIX]修改修数据job

--- src/main/java/io/choerodon/agile/infra/task/DataFixTask.java
+++ src/main/java/io/choerodon/agile/infra/task/DataFixTask.java
@@ -21,16 +21,16 @@
 //
 //
 //    @JobTask(maxRetryCount = 3,
-//            code = "migrationStatusMachineAndPage",
+//            code = "fixStatusMachineAndPage",
 //            description = "升级到0.24.0,修复状态机与页面数据")
-//    @TimedTask(name = "migrationStatusMachineAndPage",
+//    @TimedTask(name = "fixStatusMachineAndPage",
 //            description = "升级到0.24.0,修复状态机与页面数据",
 //            oneExecution = true,
 //            repeatCount = 0,
 //            repeatInterval = 1,
 //            repeatIntervalUnit = QuartzDefinition.SimpleRepeatIntervalUnit.HOURS,
 //            params = {})
-//    public void migrationStatusMachineAndPage(Map<String, Object> map) {
+//    public void fixStatusMachineAndPage(Map<String, Object> map) {
 //        LOGGER.info("==============================>>>>>>>> AGILE Data Fix Start <<<<<<<<=================================");
 //        fixDataService.fixDateStateMachineAndPage();
 //    }
