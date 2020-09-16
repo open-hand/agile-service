e2f000d0127194807b32e5aa533c61d2c9fc833c
zhaotianxin <ztxemail@163.com>
2020-09-16 19:22:41 +0800

    [FIX] 修改删除issue逻辑

--- src/main/java/io/choerodon/agile/app/service/impl/IssueServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/IssueServiceImpl.java
@@ -703,7 +703,10 @@ public class IssueServiceImpl implements IssueService {
             if (issueDTOList != null && !issueDTOList.isEmpty()) {
                 issueDTOList.forEach(subIssue -> deleteIssue(subIssue.getProjectId(), subIssue.getIssueId()));
             }
-            issueMapper.updateSubBugRelateIssueId(projectId,issueId);
+        }
+        // 清除故事或者任务与子bug的关联
+        if ("task".equals(issueConvertDTO.getTypeCode()) || "story".equals(issueConvertDTO.getTypeCode())) {
+            issueMapper.updateSubBugRelateIssueId(projectId, issueId);
         }
         //删除日志信息
         dataLogDeleteByIssueId(projectId, issueId);
