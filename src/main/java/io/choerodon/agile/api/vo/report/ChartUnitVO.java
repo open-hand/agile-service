ee0c1055a3a55414fc1ea10b97e9adcd4cf0fa5e
jiaxu.cui <hand1234>
2020-09-17 15:57:26 +0800

    [FIX] 图表添加基础校验

--- src/main/java/io/choerodon/agile/api/vo/report/ChartUnitVO.java
+++ src/main/java/io/choerodon/agile/api/vo/report/ChartUnitVO.java
@@ -70,6 +70,7 @@ public class ChartUnitVO extends ReportUnitVO {
      * 各图表必输校验
      */
     public void validate(){
+        super.validate();
         Assert.notNull(chartSearchVO, BaseConstants.ErrorCode.DATA_INVALID);
         Assert.notNull(chartSearchVO.getProjectId(), BaseConstants.ErrorCode.DATA_INVALID);
 
