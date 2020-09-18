6f1e7cf5cc3fb802952975ed5f3552ecbe2b2173
laincarl <1047972853@qq.com>
2020-09-18 14:09:59 +0800

    [ADD]修改列表渲染

--- react/routes/project-report/report-page/components/block-list/index.tsx
+++ react/routes/project-report/report-page/components/block-list/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { observer } from 'mobx-react-lite';
+import { Observer } from 'mobx-react-lite';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useProjectReportContext } from '../../context';
 import ReportBlock from '../report-block';
@@ -20,23 +20,29 @@ const BlockList: React.FC = () => {
             {...provided.droppableProps}
             ref={provided.innerRef}
           >
-            {store.blockList.map((block, index) => (
-              <Draggable key={block.key} draggableId={block.key} index={index}>
-                {(innerProvided) => (
-                  <div
-                    ref={innerProvided.innerRef}
-                    {...innerProvided.draggableProps}
-                    {...innerProvided.dragHandleProps}
-                  >
-                    <ReportBlock index={index} data={block} />
-                  </div>
-                )}
-              </Draggable>
-            ))}
+            <Observer>
+              {() => (
+                <>
+                  {store.blockList.map((block, index) => (
+                    <Draggable key={block.key} draggableId={block.key} index={index}>
+                      {(innerProvided) => (
+                        <div
+                          ref={innerProvided.innerRef}
+                          {...innerProvided.draggableProps}
+                          {...innerProvided.dragHandleProps}
+                        >
+                          <ReportBlock index={index} data={block} />
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </>
+              )}
+            </Observer>
           </div>
         )}
       </Droppable>
     </DragDropContext>
   );
 };
-export default observer(BlockList);
+export default BlockList;
