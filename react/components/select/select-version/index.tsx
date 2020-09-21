d25fe46073d8a5cdf48ae10373dbf4c12ced68f8
DingZhicheng <zhicheng.ding@hand-china.com>
2020-09-21 16:33:22 +0800

    [FIX]修复通用版本下拉列表ref数据引用丢失

--- react/components/select/select-version/index.tsx
+++ react/components/select/select-version/index.tsx
@@ -7,17 +7,16 @@ interface Props {
   dataRef: React.MutableRefObject<Array<any>>
   statusArr: Array<string>
 }
-const SelectVersion: React.FC<Props> = forwardRef(({ dataRef, statusArr = [], ...otherProps }, ref: React.Ref<Select>) => {
+const SelectVersion: React.FC<Props> = forwardRef(({ dataRef = { current: null }, statusArr = [], ...otherProps }, ref: React.Ref<Select>) => {
   const config = useMemo((): SelectConfig => ({
     name: 'version',
     textField: 'name',
     valueField: 'name',
     request: () => versionApi.loadNamesByStatus(statusArr),
     middleWare: (versions) => {
-      if (dataRef?.current) {
-        // eslint-disable-next-line no-param-reassign
-        dataRef.current = versions;
-      }
+      // eslint-disable-next-line no-param-reassign
+      dataRef.current = versions;
+
       return versions;
     },
     paging: false,
