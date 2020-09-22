c8d51273c3aa4ce278eae9924b87ef8dda24bbf5
laincarl <1047972853@qq.com>
2020-09-22 09:25:46 +0800

    [ADD]支持冲刺报告选择项目

--- react/components/charts/sprint/useSprintReport.tsx
+++ react/components/charts/sprint/useSprintReport.tsx
@@ -1,6 +1,6 @@
 import useBurnDownReport, { BurnDownConfig } from '../burn-down/useBurnDownReport';
 
-export type SprintConfig = Pick<BurnDownConfig, 'sprintId' | 'restDayShow'>
+export type SprintConfig = Pick<BurnDownConfig, 'sprintId' | 'restDayShow' | 'projectId'>
 
 function useSprintReport(config?: SprintConfig) {
   return useBurnDownReport({
