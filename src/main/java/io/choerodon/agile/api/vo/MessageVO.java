4a22a0309f471b497ebd3dd39290054a488f52fd
jiaxu.cui <hand1234>
2020-09-27 17:04:54 +0800

    [FIX] 移除多余import

--- src/main/java/io/choerodon/agile/api/vo/MessageVO.java
+++ src/main/java/io/choerodon/agile/api/vo/MessageVO.java
@@ -1,7 +1,6 @@
 package io.choerodon.agile.api.vo;
 
 import io.swagger.annotations.ApiModelProperty;
-import org.hzero.starter.keyencrypt.core.Encrypt;
 
 import javax.persistence.Transient;
 import java.util.List;
