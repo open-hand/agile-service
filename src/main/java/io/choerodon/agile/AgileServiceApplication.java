d0f940886b7a2fcac4429e8ab9f66125236d5fdf
黄福强 <fuqiang.huang01@hand-china.com>
2020-09-30 16:24:13 +0800

    Update AgileServiceApplication.java

--- src/main/java/io/choerodon/agile/AgileServiceApplication.java
+++ src/main/java/io/choerodon/agile/AgileServiceApplication.java
@@ -15,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 public class AgileServiceApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(AgileServiceApplication.class);
     }
