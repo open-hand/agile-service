0aef71a694c29f7d79a5cc6715820e898c9f58fb
superlee <superleader8@gmail.com>
2020-10-20 09:28:08 +0800

    [ADD] 添加teamProjects字段

--- src/main/java/io/choerodon/agile/api/vo/ExportIssuesVO.java
+++ src/main/java/io/choerodon/agile/api/vo/ExportIssuesVO.java
@@ -81,8 +81,17 @@ public class ExportIssuesVO {
     private Date estimatedStartTime;
     @ApiModelProperty("预计结束时间")
     private Date estimatedEndTime;
+    private String teamProjects;
 
     public ExportIssuesVO() {
+    }
+
+    public String getTeamProjects() {
+        return teamProjects;
+    }
+
+    public void setTeamProjects(String teamProjects) {
+        this.teamProjects = teamProjects;
     }
 
     public String getFeature() {
