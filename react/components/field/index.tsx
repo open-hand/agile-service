c0d862403cfcea18504c6247696829157c2eca5b
laincarl <1047972853@qq.com>
2020-10-21 09:24:27 +0800

    [FIX]更改扁平方案

--- react/components/field/index.tsx
+++ react/components/field/index.tsx
@@ -22,8 +22,7 @@ const Field: React.FC<IFieldProps> = ({
   field, mode, value, onChange, render, ...otherProps
 }) => {
   const { fieldType, title } = field;
-  const isFilter = mode === 'filter';
-  const element = render ? render(field, getFieldElement(field, isFilter)) : getFieldElement(field, isFilter);
+  const element = render ? render(field, getFieldElement(field)) : getFieldElement(field);
   const shouldMultipleOnFilter = useMemo(() => mode === 'filter'
     && ['member', 'radio', 'single', 'checkbox', 'multiple'].includes(fieldType),
   [fieldType, mode]);
