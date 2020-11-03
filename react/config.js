e0500cdfe34a57594f0fbed5a7ff72773eef161f
laincarl <1047972853@qq.com>
2020-11-03 16:21:27 +0800

    [IMP]修改配置

--- react/config.js
+++ react/config.js
@@ -1,14 +1,6 @@
 const config = {
-  local: true,
-  // use for c7n start
-  clientId: 'localhost',
-  master: './node_modules/@choerodon/master/lib/master.js',
-  projectType: 'choerodon',
-  buildType: 'single',
   modules: [
     '.',
   ],
-  dashboard: {},
-  resourcesLevel: ['site', 'origanization', 'project', 'user'],
 };
 module.exports = config;
