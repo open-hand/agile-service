518998f0284311c9dea851007cad312886fc15a5
812913 <lwfvirgo@163.com>
2020-11-05 10:16:12 +0800

    [IMP] 修改迭代计划创建文图默认选择的冲刺不正确的问题

--- react/routes/ScrumBoard/ScrumBoardComponent/create-issue/index.js
+++ react/routes/ScrumBoard/ScrumBoardComponent/create-issue/index.js
@@ -7,26 +7,29 @@ import scrumBoardStore from '@/stores/project/scrumBoard/ScrumBoardStore';
 // interface Props {
 //   refresh: (boardId: string) => void,
 // }
-const ScrumBoardCreateIssue = ({ refresh }) => (
-  scrumBoardStore.getCreateIssueVisible ? (
-    <CreateIssue
-      visible={scrumBoardStore.getCreateIssueVisible}
-      onCancel={() => {
-        scrumBoardStore.setCreateIssueVisible(false);
-      }}
-      chosenSprint={scrumBoardStore.getSprintId}
-      onOk={(res, requestData) => {
-        scrumBoardStore.setCreateIssueVisible(false);
-        // // 创建issue后刷新
-        const { sprintId } = requestData;
-        if (scrumBoardStore.getSprintId && String(sprintId) === String(scrumBoardStore.getSprintId)) {
-          refresh(scrumBoardStore.getBoardList.get(scrumBoardStore.getSelectedBoard));
-        }
+const ScrumBoardCreateIssue = ({ refresh }) => {
+  const doingSprint = scrumBoardStore.didCurrentSprintExist ? scrumBoardStore.sprintNotClosedArray.find((item) => item.statusCode === 'started') : {};
+  return (
+    scrumBoardStore.getCreateIssueVisible ? (
+      <CreateIssue
+        visible={scrumBoardStore.getCreateIssueVisible}
+        onCancel={() => {
+          scrumBoardStore.setCreateIssueVisible(false);
+        }}
+        chosenSprint={scrumBoardStore.quickSearchObj && scrumBoardStore.quickSearchObj.sprintId ? scrumBoardStore.quickSearchObj.sprintId : doingSprint.sprintId}
+        onOk={(res, requestData) => {
+          scrumBoardStore.setCreateIssueVisible(false);
+          // // 创建issue后刷新
+          const { sprintId } = requestData;
+          if (scrumBoardStore.getSprintId && String(sprintId) === String(scrumBoardStore.getSprintId)) {
+            refresh(scrumBoardStore.getBoardList.get(scrumBoardStore.getSelectedBoard));
+          }
         // if (res) {
         //   BacklogStore.refresh(false, false);
         // }
-      }}
-    />
-  ) : null
-);
+        }}
+      />
+    ) : null
+  );
+};
 export default observer(ScrumBoardCreateIssue);
