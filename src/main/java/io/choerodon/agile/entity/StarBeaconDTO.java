fcf91881174daa7839377bb902c0c18808453954
yukdawn <yukdawn@gmail.com>
2020-11-09 17:24:10 +0800

    [ADD] 添加星标设计代码框架

---
+++ src/main/java/io/choerodon/agile/entity/StarBeaconDTO.java
@@ -0,0 +1,79 @@
+package io.choerodon.agile.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.choerodon.mybatis.annotation.ModifyAudit;
+import io.choerodon.mybatis.annotation.VersionAudit;
+import io.choerodon.mybatis.domain.AuditDomain;
+import io.swagger.annotations.ApiModel;
+
+/**
+ * 星标表
+ *
+ * @author jiaxu.cui@hand-china.com 2020-11-09 17:08:45
+ */
+@ApiModel("星标表")
+@VersionAudit
+@ModifyAudit
+@Table(name = "fd_star_beacon")
+public class StarBeaconDTO extends AuditDomain {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String type;
+    private Long instanceId;
+    private Long projectId;
+    private Long organizationId;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(Long instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Long getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Long organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/io/choerodon/agile/mapper/StarBeaconMapper.java
@@ -0,0 +1,14 @@
+package io.choerodon.agile.mapper;
+
+import io.choerodon.agile.entity.StarBeaconDTO;
+import io.choerodon.mybatis.common.BaseMapper;
+
+/**
+ * Mapper
+ *
+ * @author jiaxu.cui@hand-china.com 2020-11-09 17:08:45
+ */
+public interface StarBeaconMapper extends BaseMapper<StarBeaconDTO> {
+
+}
+
