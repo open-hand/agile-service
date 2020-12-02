522bec1120500f95c1d869bb5f11b2e1d2e321fa
laincarl <1047972853@qq.com>
2020-12-02 09:28:33 +0800

    [IMP]优化加载和文案

--- react/components/select/select-sprint/index.tsx
+++ react/components/select/select-sprint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, forwardRef, useRef } from 'react';
+import React, { useMemo, forwardRef } from 'react';
 import { Select } from 'choerodon-ui/pro';
 import { Tooltip } from 'choerodon-ui';
 import { sprintApi } from '@/api';
@@ -31,18 +31,14 @@ const SelectSprint: React.FC<Props> = forwardRef(({
   flat,
   ...otherProps
 }, ref: React.Ref<Select>) => {
-  const afterLoadRef = useRef<Function>();
-  afterLoadRef.current = afterLoad;
   const config = useMemo((): SelectConfig<ISprint> => ({
     name: 'sprint',
     textField: 'sprintName',
     valueField: 'sprintId',
+    afterLoad,
     request: ({ filter, page }) => (isProgram ? sprintApi.loadSubProjectSprints(filter || '', page!, selectSprints)
       : sprintApi.project(projectId).loadSprints(statusList)),
     middleWare: (sprints) => {
-      if (afterLoadRef.current) {
-        afterLoadRef.current(sprints);
-      }
       let newSprint = sprints;
       if (hasUnassign) {
         newSprint = [{ sprintId: '0', sprintName: '未分配冲刺', endDate: '' } as ISprint, ...sprints];
