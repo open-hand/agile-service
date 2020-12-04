949399031defc9404b8db8b53126d34b3d84dda1
DingZhicheng <zhicheng.ding@hand-china.com>
2020-12-04 19:29:38 +0800

    [IMP]状态机增加一键选择box

--- react/routes/StateMachine/status-circulation/components/status-circulation-table/index.tsx
+++ react/routes/StateMachine/status-circulation/components/status-circulation-table/index.tsx
@@ -3,12 +3,13 @@ import {
   Button, Modal, Spin, Tooltip,
 } from 'choerodon-ui/pro';
 import Measure from 'react-measure';
-import { observer } from 'mobx-react-lite';
+import { observer, Observer, useComputed } from 'mobx-react-lite';
 import STATUS from '@/constants/STATUS';
 import { IStatusCirculation } from '@/api';
 import Table, { ColumnsType } from 'antd/lib/table';
 import 'antd/lib/table/style';
 import { useStateMachineContext } from '@/routes/StateMachine/context';
+import CheckboxAll from './check-box-all';
 import { useStatusCirculationContext } from '../..';
 import Checkbox from './Checkbox';
 import DeleteStatus from './DeleteStatus';
@@ -19,6 +20,26 @@ const StatusCirculationTable: React.FC = () => {
   const { store } = useStatusCirculationContext();
   const { selectedType } = useStateMachineContext();
   const { statusList, loading } = store;
+  function judgeRowCheckAll(statusId: string, record: IStatusCirculation): boolean {
+    const statusActions = store.actions.get(statusId);
+    if (statusActions && statusActions.some((item) => item.type === 'nocheck')) {
+      return false;
+    }
+    if (record.canTransformStatus.length !== statusList.length) {
+      return false;
+    }
+    return true;
+  }
+  function judgeRowCheckSome(statusId: string, record: IStatusCirculation): boolean { /**   */
+    const statusActions = store.actions.get(statusId);
+    if (statusActions && statusActions.length > 0 && statusActions.some((item) => item.type === 'check')) {
+      return true;
+    }
+    if (record.canTransformStatus.length === statusList.length) {
+      return true;
+    }
+    return false;
+  }
   const handleDeleteClick = useCallback((record: IStatusCirculation) => {
     Modal.open({
       title: `确认删除状态“${record.name}”`,
@@ -36,7 +57,35 @@ const StatusCirculationTable: React.FC = () => {
   const statusColumns: ColumnsType<IStatusCirculation> = useMemo(() => statusList.map((status) => ({
     dataIndex: status.name,
     width: 150,
-    title: <span style={{ color: STATUS[status.type], fontWeight: 500 }}>{status.name}</span>,
+    title: (
+      <span style={{ color: STATUS[status.type], fontWeight: 500 }}>
+        <Observer>
+          {() => {
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const currentSize = useComputed(() => store.checkedMaps.get(status.id)?.columnCurrentSize, [store.checkedMaps.get(status.id)?.columnCurrentSize]);
+            console.log(`render column ${status.name} -- size:${currentSize}`);
+            return (
+              <CheckboxAll
+                name={`column-${status.name}`}
+                // indeterminate=
+                record={status}
+                indeterminate={!!(currentSize && currentSize > 1 && currentSize < store.statusList.length)}
+                checked={currentSize === store.statusList.length}
+                onChange={(value) => {
+                  store.statusList.forEach((item) => {
+                    if (item.id !== status.id) {
+                      store.checkChange(item.id, status.id, value);
+                    }
+                  });
+                  return value;
+                }}
+              />
+            );
+          }}
+
+        </Observer>
+        {status.name}
+      </span>),
     render: ((text: string, record) => (
       <Checkbox store={store} status={status} record={record} />
     )),
@@ -48,6 +97,30 @@ const StatusCirculationTable: React.FC = () => {
     title: null,
     render: ((text: string, record) => (
       <span style={{ color: STATUS[record.type], fontWeight: 500 }}>
+        <Observer>
+          {() => {
+            // eslint-disable-next-line react-hooks/rules-of-hooks
+            const rowInfo = useComputed(() => store.checkedMaps.get(record.id)!, [store.checkedMaps.get(record.id)]);
+            console.log(`render row ${record.name} -- checked:${rowInfo.rowChecked} indeterminate ${rowInfo.rowIndeterminate}`);
+
+            return (
+              <CheckboxAll
+                name={`row-${record.name}`}
+                record={record}
+                indeterminate={rowInfo.rowIndeterminate}
+                checked={rowInfo.rowChecked}
+                onChange={(value) => {
+                  store.statusList.forEach((item) => {
+                    if (item.id !== record.id) {
+                      store.checkChange(record.id, item.id, value);
+                    }
+                  });
+                  return value;
+                }}
+              />
+            );
+          }}
+        </Observer>
         {record.name}
         {record.defaultStatus && <span className={styles.default_status}>初始</span>}
       </span>
