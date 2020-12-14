9a075f259a793107675fced78ae2e52b71f796df
laincarl <1047972853@qq.com>
2020-12-14 09:53:38 +0800

    [IMP]故事地图增加提示

--- react/routes/StoryMap/StoryMapHome/components/Search/Search.tsx
+++ react/routes/StoryMap/StoryMapHome/components/Search/Search.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback } from 'react';
+import React from 'react';
+import { Button } from 'choerodon-ui/pro';
 import { observer } from 'mobx-react-lite';
-import { toJS } from 'mobx';
 import IssueSearch, { IssueSearchStore } from '@/components/issue-search';
 import StoryMapStore from '@/stores/project/StoryMap/StoryMapStore';
 import { localPageCacheStore } from '@/stores/common/LocalPageCacheStore';
 import openSaveFilterModal from '@/components/SaveFilterModal';
+import { Tooltip } from 'choerodon-ui';
 import styles from './Search.less';
 
 interface Props {
@@ -35,6 +36,19 @@ const StoryMapSearch: React.FC<Props> = ({ issueSearchStore }) => {
         }}
         // onClickSaveFilter={handleClickSaveFilter}
       />
+      <Tooltip title="故事地图默认显示最近5个冲刺/版本，您可以使用筛选选择其他或者更多的冲刺/版本。" placement="bottomLeft">
+        <Button
+          icon="help"
+          style={{
+            color: '#5365EA',
+            border: '1px solid #D9E6F2',
+            fontWeight: 500,
+            borderRadius: '6px',
+            margin: '8px 15px 0px 10px',
+            flexShrink: 0,
+          }}
+        />
+      </Tooltip>
     </div>
 
   );
