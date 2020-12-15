85bcc90d7ce7ff06b4bdd866febbddef9aac9b75
hulingfangzi <lingfangzi.hu01@hand-china.com>
2020-12-15 11:01:07 +0800

    [ADD]添加团队绩效

---
+++ react/routes/TeamPerformance/Performance/TeamPerformance.js
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { Tabs } from 'choerodon-ui/pro';
+import {
+  Page, Content, Breadcrumb,
+} from '@choerodon/boot';
+import Efficiency from '../components/Efficiency';
+import Quality from '../components/Quality';
+import Tendency from '../components/Tendency';
+import Store from '../stores';
+import './index.less';
+
+const { TabPane } = Tabs;
+const GroupPerformance = () => {
+  const { prefixCls } = useContext(Store);
+
+  return (
+    <Page className={prefixCls}>
+      <Breadcrumb />
+      <Content>
+        <Tabs>
+          <TabPane key="efficiency" tab="进度与效率">
+            <div className={`${prefixCls}-container`}>
+              <Efficiency />
+            </div>
+          </TabPane>
+          <TabPane key="quality" tab="质量分析">
+            <div className={`${prefixCls}-container`}>
+              <Quality />
+            </div>
+          </TabPane>
+          <TabPane key="tendency" tab="趋势分析">
+            <div className={`${prefixCls}-container`}>
+              <Tendency />
+            </div>
+          </TabPane>
+        </Tabs>
+      </Content>
+    </Page>
+  );
+};
+
+export default GroupPerformance;
---
+++ react/routes/TeamPerformance/components/Quality/BugBarChart.js
@@ -0,0 +1,198 @@
+// 质量分析-柱状图
+import React, { useContext, useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Spin, Form, Select } from 'choerodon-ui/pro';
+import ReactEchartsCore from 'echarts-for-react/lib/core';
+import echarts from 'echarts';
+import SwitchTabs from '../SwitchTabs';
+import EmptyBlock from '../EmptyBlock';
+import Store from '../../stores';
+import emptyChartPic from '../../image/empty_chart.svg';
+import './index.less';
+
+const BugBarChart = observer(() => {
+  const { bugChartHandleDataSet, bugDistributionDataSet } = useContext(Store);
+  const [barData, setBarData] = useState(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    queryBarData();
+  }, []);
+
+  const queryBarData = async () => {
+    const { type, environment } = bugChartHandleDataSet.current.toData();
+    bugDistributionDataSet.setQueryParameter('environment', environment);
+    bugDistributionDataSet.setQueryParameter('type', type);
+    await bugDistributionDataSet.query();
+    setShow(bugDistributionDataSet.length > 10);
+    setBarData(bugDistributionDataSet.toData());
+  };
+
+  const getOpts = () => ({
+    legend: {
+      show: false,
+    },
+    dataset: {
+      source: barData,
+    },
+    tooltip: {
+      formatter(params) {
+        let content = '';
+        content = `<div>
+         <div style="display:inline-block; width: 10px; height: 10px; margin-right: 3px; border-radius: 50%; background:${params.color}"></div>
+         ${params.data.realName}：${params.data.bugCount}
+         </div>`;
+        return content;
+      },
+      textStyle: {
+        color: '#000',
+        fontSize: 12,
+        lineHeight: 20,
+      },
+      extraCssText: 'background: #FFFFFF;\n'
+        + 'border: 1px solid #DDDDDD;\n'
+        + 'box-shadow: 0 2px 4px 0 rgba(0,0,0,0.20);\n'
+        + 'border-radius: 0;\n'
+        + 'padding: 8px',
+    },
+    grid: {
+      top: 50,
+      left: 8,
+      right: 12,
+      bottom: 30,
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'category',
+      axisTick: { show: false },
+      interval: 0,
+      axisLine: {
+        show: false,
+        lineStyle: {
+          color: '#eee',
+        },
+        onZero: true,
+      },
+      splitLine: {
+        show: false,
+      },
+      axisLabel: {
+        show: true,
+        textStyle: {
+          color: 'rgba(0, 0, 0, 0.65)',
+          fontSize: 12,
+          fontStyle: 'normal',
+        },
+      },
+    },
+    yAxis: {
+      name: '问题计数',
+      nameTextStyle: {
+        color: '#000',
+        padding: [0, 10, 0, 0],
+      },
+      minInterval: 1,
+      nameGap: 23,
+      axisTick: { show: false },
+      axisLine: { show: false },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: 'rgba(238, 238, 238, 1)',
+        },
+      },
+      axisLabel: {
+        show: true,
+        textStyle: {
+          color: 'rgba(0, 0, 0, 0.65)',
+          fontSize: 12,
+          fontStyle: 'normal',
+        },
+      },
+    },
+    series: [
+      {
+        type: 'bar',
+        color: 'rgba(249, 136, 148, 1)',
+        barWidth: 10,
+        itemStyle: {
+          barBorderRadius: [5, 5, 0, 0],
+        },
+        dimensions: [
+          { name: 'realName', type: 'ordinal' },
+          { name: 'bugCount', type: 'number' },
+        ],
+      },
+    ],
+    dataZoom: [{
+      show,
+      type: 'slider',
+      bottom: 6,
+      left: 100,
+      height: 15,
+      width: '82%',
+      startValue: 0,
+      endValue: 9,
+      zoomLock: true,
+      handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      handleSize: '100%',
+      handleStyle: {
+        color: '#fff',
+        borderType: 'dashed',
+        shadowBlur: 4,
+        shadowColor: 'rgba(0, 0, 0, 0.6)',
+        shadowOffsetX: 2,
+        shadowOffsetY: 2,
+      },
+    }],
+  });
+
+  return (
+    <Spin dataSet={bugDistributionDataSet}>
+      <div className="bar-chart-container">
+        <div className="chart-handle">
+          <span className="chart-title">缺陷分布</span>
+          <SwitchTabs
+            dataSet={bugChartHandleDataSet}
+            field="type"
+            onChange={() => queryBarData()}
+            style={{ marginLeft: '40px' }}
+          />
+          <Form dataSet={bugChartHandleDataSet} style={{ width: '130px', marginLeft: '15px', marginTop: '5px' }}>
+            <Select
+              name="environment"
+              clearButton={false}
+              onChange={() => queryBarData()}
+            />
+          </Form>
+        </div>
+        {
+          (barData && barData.length > 0) && (
+            <div className="line-chart-main">
+              <ReactEchartsCore
+                echarts={echarts}
+                option={getOpts()}
+                notMerge
+                lazyUpdate
+                style={{
+                  height: 330,
+                }}
+              />
+            </div>
+          )
+        }
+        {
+          (Array.isArray(barData) && barData.length === 0) && (
+            <EmptyBlock
+              pic={emptyChartPic}
+              height={330}
+              des="当前暂无数据"
+            />
+          )
+        }
+      </div>
+    </Spin>
+  );
+});
+
+export default BugBarChart;
