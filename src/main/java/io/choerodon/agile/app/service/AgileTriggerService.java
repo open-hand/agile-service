4cbd37bcc1662b2c808f566ac4beb891b06f7d32
superlee <superleader8@gmail.com>
2020-12-20 11:51:19 +0800

    [ADD] 删除issue同时删除ruleLogRel

--- src/main/java/io/choerodon/agile/app/service/AgileTriggerService.java
+++ src/main/java/io/choerodon/agile/app/service/AgileTriggerService.java
@@ -25,4 +25,6 @@ public interface AgileTriggerService {
      * @return
      */
     List<RuleLogRelVO> queryRuleLogRelList(RuleLogRelVO ruleLogRelVO);
+
+    void delete(RuleLogRelVO ruleLogRel);
 }
