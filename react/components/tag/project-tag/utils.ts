8274ec717aef5b7cf02f537359c06a367e68c4d2
laincarl <1047972853@qq.com>
2020-12-23 13:35:00 +0800

    [FIX]修复项目tag

--- react/components/tag/project-tag/utils.ts
+++ react/components/tag/project-tag/utils.ts
@@ -1,5 +1,5 @@
 export function getColorByNumber(id: string | number) {
-  const index = Number(String(BigInt(id) % BigInt(4)));
+  const index = isNaN(id as number) ? 0 : Number(String(BigInt(id) % BigInt(4)));
   const valiable = [
     'linear-gradient(225deg,rgba(152,229,218,1) 0%,rgba(0,191,165,1) 100%)',
     'linear-gradient(226deg,rgba(255,212,163,1) 0%,rgba(255,185,106,1) 100%)',
