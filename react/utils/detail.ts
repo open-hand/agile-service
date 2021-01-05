604be8b5a6abd4b510cbb01eb8fc8e793596ea03
laincarl <1047972853@qq.com>
2021-01-05 10:31:03 +0800

    [IMP]项目群不可编辑子项目issue

--- react/utils/detail.ts
+++ react/utils/detail.ts
@@ -6,21 +6,21 @@ export function sameProject(projectId: string): boolean {
 }
 export function disableIssueEdit(projectId: string): boolean {
   const same = String(projectId) === String(getProjectId());
-  const isInProgram = getApplyType() === 'program';
-  // 项目群可编辑子项目issue
-  if (isInProgram) {
-    return false;
-  }
+  // const isInProgram = getApplyType() === 'program';
+  // // 项目群可编辑子项目issue
+  // if (isInProgram) {
+  //   return false;
+  // }
   // 子项目之间不可互相编辑，也不可编辑项目群issue
   return !same;
 }
 export function disableDemandEdit(projectId: string): boolean {
   const same = String(projectId) === String(getProjectId());
-  const isInProgram = getApplyType() === 'program';
-  // 项目群可编辑子项目issue
-  if (isInProgram) {
-    return false;
-  }
+  // const isInProgram = getApplyType() === 'program';
+  // // 项目群可编辑子项目issue
+  // if (isInProgram) {
+  //   return false;
+  // }
   // 子项目之间不可互相编辑，也不可编辑项目群issue
   return !same;
 }
