bcf0a4c262fe275530d0d3f17a9e75c8a3703bbd
superlee <superleader8@gmail.com>
2021-01-14 15:16:29 +0800

    [ADD] 添加消费项目更新事件的sagaTask

--- src/main/java/io/choerodon/agile/app/eventhandler/AgileEventHandler.java
+++ src/main/java/io/choerodon/agile/app/eventhandler/AgileEventHandler.java
@@ -1,6 +1,7 @@
 package io.choerodon.agile.app.eventhandler;
 
 import com.alibaba.fastjson.JSON;
+import io.choerodon.agile.api.vo.ProjectInfoVO;
 import io.choerodon.agile.api.vo.event.OrganizationCreateEventPayload;
 import io.choerodon.agile.api.vo.event.ProjectEvent;
 import io.choerodon.agile.api.vo.event.ProjectEventCategory;
@@ -22,8 +23,7 @@ import java.util.stream.Collectors;
 
 import static io.choerodon.agile.infra.utils.SagaTopic.Organization.ORG_CREATE;
 import static io.choerodon.agile.infra.utils.SagaTopic.Organization.TASK_ORG_CREATE;
-import static io.choerodon.agile.infra.utils.SagaTopic.Project.PROJECT_CREATE;
-import static io.choerodon.agile.infra.utils.SagaTopic.Project.TASK_PROJECT_CREATE;
+import static io.choerodon.agile.infra.utils.SagaTopic.Project.*;
 
 /**
  * Created by HuangFuqiang@choerodon.io on 2018/5/22.
@@ -86,28 +86,56 @@ public class AgileEventHandler {
         LOGGER.info("接受创建项目消息{}", message);
         List<ProjectEventCategory> projectEventCategories = projectEvent.getProjectCategoryVOS();
         if (!ObjectUtils.isEmpty(projectEventCategories)) {
-            Set<String> codes =
-                    projectEventCategories
-                            .stream()
-                            .map(ProjectEventCategory::getCode)
-                            .collect(Collectors.toSet());
-            if (ProjectCategory.consumeProjectCreatEvent(codes)) {
-                //创建projectInfo
-                projectInfoService.initializationProjectInfo(projectEvent);
-                //创建项目初始化issueLinkType
-                issueLinkTypeService.initIssueLinkType(projectEvent.getProjectId());
-                if (codes.contains(ProjectCategory.MODULE_PROGRAM)) {
-                    AgilePluginService pluginService = SpringBeanUtil.getExpandBean(AgilePluginService.class);
-                    if (pluginService != null) {
-                        pluginService.initProjectIssueTypeSchemeAndArt(projectEvent);
-                    }
-                } else {
-                    //创建项目时创建默认状态机方案
-                    stateMachineSchemeService.initByConsumeCreateProject(projectEvent);
-                    //创建项目时创建默认问题类型方案
-                    issueTypeSchemeService.initByConsumeCreateProject(projectEvent.getProjectId(), projectEvent.getProjectCode());
+            initIfAgileProject(projectEvent, projectEventCategories);
+        }
+        return message;
+    }
+
+    private void initIfAgileProject(ProjectEvent projectEvent, List<ProjectEventCategory> projectEventCategories) {
+        Set<String> codes =
+                projectEventCategories
+                        .stream()
+                        .map(ProjectEventCategory::getCode)
+                        .collect(Collectors.toSet());
+        if (ProjectCategory.consumeProjectCreatEvent(codes)) {
+            LOGGER.info("初始化项目{}, code: {}", projectEvent.getProjectId(), projectEvent.getProjectCode());
+            //创建projectInfo
+            projectInfoService.initializationProjectInfo(projectEvent);
+            //创建项目初始化issueLinkType
+            issueLinkTypeService.initIssueLinkType(projectEvent.getProjectId());
+            if (codes.contains(ProjectCategory.MODULE_PROGRAM)) {
+                AgilePluginService pluginService = SpringBeanUtil.getExpandBean(AgilePluginService.class);
+                if (pluginService != null) {
+                    pluginService.initProjectIssueTypeSchemeAndArt(projectEvent);
                 }
+            } else {
+                //创建项目时创建默认状态机方案
+                stateMachineSchemeService.initByConsumeCreateProject(projectEvent);
+                //创建项目时创建默认问题类型方案
+                issueTypeSchemeService.initByConsumeCreateProject(projectEvent.getProjectId(), projectEvent.getProjectCode());
             }
+        }
+    }
+
+    /**
+     * 更新项目事件
+     *
+     * @param message message
+     */
+    @SagaTask(code = TASK_PROJECT_UPDATE, sagaCode = PROJECT_UPDATE,seq = 2,
+            description = "agile消费更新项目事件初始化项目数据")
+    public String handleProjectUpdateByConsumeSagaTask(String message) {
+        ProjectEvent projectEvent = JSON.parseObject(message, ProjectEvent.class);
+        LOGGER.info("接受更新项目消息{}", message);
+        Long projectId = projectEvent.getProjectId();
+        ProjectInfoVO projectInfo = projectInfoService.queryProjectInfoByProjectId(projectId);
+        if (projectInfo == null) {
+            List<ProjectEventCategory> projectEventCategories = projectEvent.getProjectCategoryVOS();
+            if (!ObjectUtils.isEmpty(projectEventCategories)) {
+                initIfAgileProject(projectEvent, projectEventCategories);
+            }
+        } else {
+            LOGGER.info("项目{}已初始化，跳过项目初始化", projectEvent.getProjectCode());
         }
         return message;
     }
