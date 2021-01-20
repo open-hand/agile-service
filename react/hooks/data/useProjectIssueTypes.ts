650ef95ca46093e4698d33ef3c0095761bfd1486
laincarl <1047972853@qq.com>
2021-01-20 15:01:53 +0800

    [IMP]修改问题类型渲染

--- react/hooks/data/useProjectIssueTypes.ts
+++ react/hooks/data/useProjectIssueTypes.ts
@@ -7,7 +7,7 @@ import useProjectKey from './useProjectKey';
 export interface ProjectIssueTypesConfig {
   projectId?: string
   /** 只返回某一类的问题类型 */
-  typeCode?: string
+  typeCode?: string | string[]
 }
 export default function useProjectIssueTypes(config?: ProjectIssueTypesConfig, options?: UseQueryOptions<IIssueType[]>) {
   const { isProgram } = useIsProgram();
@@ -16,7 +16,9 @@ export default function useProjectIssueTypes(config?: ProjectIssueTypesConfig, o
   return useQuery(key, () => issueTypeApi.loadAllWithStateMachineId(applyType), {
     select: (data) => {
       const issueTypes = (!isProgram ? data.filter((item: IIssueType) => item.typeCode !== 'feature') : data);
-      return config?.typeCode ? issueTypes.filter((type) => type.typeCode === config?.typeCode) : issueTypes;
+      // eslint-disable-next-line no-nested-ternary
+      const typeCodes = Array.isArray(config?.typeCode) ? config?.typeCode : (config?.typeCode ? [config?.typeCode] : null);
+      return typeCodes ? issueTypes.filter((type) => typeCodes.includes(type.typeCode)) : issueTypes;
     },
     placeholderData: [] as IIssueType[],
     ...options,
