eb74f72822ecab88c9614ce857c42d6b68201533
812913 <lwfvirgo@163.com>
2021-01-25 17:43:07 +0800

    [IMP] 修复不能停用的问题 & 跳转到状态机时添加issueTypeId参数

--- react/routes/issueType/issueTypeList/IssueTypeList.tsx
+++ react/routes/issueType/issueTypeList/IssueTypeList.tsx
@@ -44,8 +44,13 @@ function IssueTypeList() {
   }, [isOrganization]);
 
   const handleLinkToStatus = useCallback(() => {
-    addRef.current?.submit(() => {
-      to(LINK_URL.status, { type: isOrganization ? 'org' : 'project' });
+    addRef.current?.submit((id: string) => {
+      to(LINK_URL.status, {
+        type: isOrganization ? 'org' : 'project',
+        params: {
+          issueTypeId: id,
+        },
+      });
     });
   }, [isOrganization]);
 
@@ -138,7 +143,7 @@ function IssueTypeList() {
 
     const handleStop = () => {
       issueTypeApi.getStopDisable(record?.get('id')).then((disable: boolean) => {
-        if (disable) {
+        if (!disable) {
           Choerodon.prompt(`至少启用一个${record?.get('typeCode') === 'sub_task' ? '子级' : '父级'}问题类型`);
         } else {
           issueTypeApi.enabled(record?.get('id'), false).then(() => {
