25730b9fb004e50b612c9da64814ce169c7aed86
DingZhicheng <zhicheng.ding@hand-china.com>
2021-01-29 13:55:13 +0800

    [IMP]特殊系统字段默认值增加赋值操作

--- react/routes/page-config/page-issue-type/stores/PageIssueTypeStore.ts
+++ react/routes/page-config/page-issue-type/stores/PageIssueTypeStore.ts
@@ -230,7 +230,7 @@ class PageIssueTypeStore {
     pageConfigApi.loadByIssueType(this.getCurrentIssueType).then((res) => {
       this.sortTableDataSet.loadData(res.fields.map((field) => ({
         ...field,
-        showDefaultValueText: transformDefaultValue({ ...field, defaultValue: field.defaultValueObjs && field.defaultValueObjs.length > 0 ? field.defaultValue : undefined, fieldOptions: field.fieldOptions || field.defaultValueObjs || [field.defaultValueObj].filter((item) => item) }),
+        showDefaultValueText: transformDefaultValue({ ...field, defaultValue: !field.defaultValueObjs || (field.defaultValueObjs && field.defaultValueObjs.length > 0) ? field.defaultValue : undefined, fieldOptions: field.fieldOptions || field.defaultValueObjs || [field.defaultValueObj].filter((item) => item) }),
       })));
       if (res.issueTypeFieldVO) {
         this.setDescriptionObj({
