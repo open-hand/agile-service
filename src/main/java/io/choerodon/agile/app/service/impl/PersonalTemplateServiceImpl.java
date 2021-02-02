04d03d786205bd6ff4a9d935997bcba7aef10c40
huaxin <2502353032@qq.com>
2021-02-02 20:58:17 +0800

    [ADD] 增加校验

--- src/main/java/io/choerodon/agile/app/service/impl/PersonalTemplateServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/PersonalTemplateServiceImpl.java
@@ -3,18 +3,24 @@ package io.choerodon.agile.app.service.impl;
 import io.choerodon.agile.api.vo.PersonalTemplateCreateVO;
 import io.choerodon.agile.api.vo.PersonalTemplatelVO;
 import io.choerodon.agile.api.vo.PersonalTemplateUpdateVO;
+import io.choerodon.agile.api.vo.ProjectVO;
 import io.choerodon.agile.app.service.PersonalTemplateService;
 import io.choerodon.agile.infra.dto.PersonalTemplateDTO;
+import io.choerodon.agile.infra.enums.ProjectCategory;
 import io.choerodon.agile.infra.mapper.PersonalTemplateMapper;
+import io.choerodon.agile.infra.utils.ConvertUtil;
 import io.choerodon.core.exception.CommonException;
+import io.choerodon.core.oauth.DetailsHelper;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,6 +31,9 @@ import java.util.Objects;
 @Transactional(rollbackFor = Exception.class)
 public class PersonalTemplateServiceImpl implements PersonalTemplateService {
 
+    private static final String[] AGILE_PERSONAL_TEMPLATE_ACTIONS = {"agile_import_issue","agile_export_issue","agile_export_feature"};
+    private static final String[] PROGRAM_PERSONAL_TEMPLATE_ACTIONS = {"program_import_feature","program_export_feature"};
+    private static final String[] PERSONAL_TEMPLATE_TYPES = {"excel"};
     @Autowired
     private PersonalTemplateMapper personalTemplateMapper;
     @Autowired
@@ -35,6 +44,10 @@ public class PersonalTemplateServiceImpl implements PersonalTemplateService {
         if (checkNameByAction(projectId, createVO.getUserId(), createVO.getName(), createVO.getAction(), createVO.getType())) {
             throw new CommonException("error.personal.template.name.exist");
         }
+        //校验action合法性
+        checkAction(projectId, createVO.getAction());
+        //校验类型
+        checkType(createVO.getType());
         PersonalTemplateDTO personalTemplateDTO = modelMapper.map(createVO, PersonalTemplateDTO.class);
         if (personalTemplateMapper.insert(personalTemplateDTO) != 1) {
             throw new CommonException("error.personal.template.create");
@@ -48,6 +61,8 @@ public class PersonalTemplateServiceImpl implements PersonalTemplateService {
         if (Objects.isNull(personalTemplateDTO)) {
             throw new CommonException("error.personal.template.not.existed");
         }
+        //校验当前用户
+        checkUser(personalTemplateDTO.getUserId());
         if (!Objects.equals(personalTemplateDTO.getName(), updateVO.getName())
                 && checkNameByAction(projectId, personalTemplateDTO.getUserId(), updateVO.getName(), personalTemplateDTO.getAction(), personalTemplateDTO.getType())) {
             throw new CommonException("error.personal.template.name.exist");
@@ -66,6 +81,8 @@ public class PersonalTemplateServiceImpl implements PersonalTemplateService {
         if (Objects.isNull(personalTemplateDTO)) {
             return;
         }
+        //校验当前用户
+        checkUser(personalTemplateDTO.getUserId());
         if (personalTemplateMapper.deleteByPrimaryKey(id) != 1) {
             throw new CommonException("error.personal.template.delete");
         }
@@ -79,6 +96,10 @@ public class PersonalTemplateServiceImpl implements PersonalTemplateService {
 
     @Override
     public List<PersonalTemplatelVO> queryByUserAndAction(Long projectId, Long userId, String action, String type) {
+        //校验action合法性
+        checkAction(projectId, action);
+        //校验类型
+        checkType(type);
         PersonalTemplateDTO search = new PersonalTemplateDTO(null, action, type, projectId, userId);
         List<PersonalTemplateDTO> personalTemplateDTOList = personalTemplateMapper.select(search);
         if (CollectionUtils.isEmpty(personalTemplateDTOList)) {
@@ -90,9 +111,39 @@ public class PersonalTemplateServiceImpl implements PersonalTemplateService {
 
     @Override
     public Boolean checkNameByAction(Long projectId, Long userId, String name, String action, String type) {
+        //校验action合法性
+        checkAction(projectId, action);
+        //校验类型
+        checkType(type);
         PersonalTemplateDTO check = new PersonalTemplateDTO(null, action, type, projectId, userId);
         check.setName(name);
         PersonalTemplateDTO personalTemplateDTO = personalTemplateMapper.selectOne(check);
         return !Objects.isNull(personalTemplateDTO);
+    }
+
+    private void checkAction(Long projectId, String action) {
+        ProjectVO body = ConvertUtil.queryProject(projectId);
+        List<String> actions;
+        if (!ObjectUtils.isEmpty(body) && ProjectCategory.checkContainProjectCategory(body.getCategories(),ProjectCategory.MODULE_PROGRAM)) {
+            actions = Arrays.asList(PROGRAM_PERSONAL_TEMPLATE_ACTIONS);
+        }else {
+            actions = Arrays.asList(AGILE_PERSONAL_TEMPLATE_ACTIONS);
+        }
+        if (!actions.contains(action)) {
+            throw new CommonException("error.action.is.illegal");
+        }
+    }
+
+    private void checkUser(Long userId) {
+        Long currentUserId = DetailsHelper.getUserDetails().getUserId();
+        if (!Objects.equals(currentUserId, userId)) {
+            throw new CommonException("error.template.not.belong.current.user");
+        }
+    }
+
+    private void checkType(String type) {
+        if (!Arrays.asList(PERSONAL_TEMPLATE_TYPES).contains(type)) {
+            throw new CommonException("error.template.type.is.illegal");
+        }
     }
 }
