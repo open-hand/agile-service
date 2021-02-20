0fb7924158c32d7936451e339e497e2bc0176855
25287 <chihao.ran@hand-china.com>
2021-02-20 10:36:04 +0800

    [FIX]静态文件上传校验修改

--- src/main/java/io/choerodon/agile/app/service/impl/StaticFileCompressServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/StaticFileCompressServiceImpl.java
@@ -370,7 +370,7 @@ public class StaticFileCompressServiceImpl implements StaticFileCompressService 
                 if (entryName.contains(MACOSX) || entryName.contains(DS_STORE)) {
                     continue;
                 }
-                if (!entry.isDirectory() && entryName.contains(INDEX_HTML)) {
+                if (!entry.isDirectory() && INDEX_HTML.equals(entryName)) {
                     notIndex = false;
                     result = getPrefixPath(entry.getName());
                 }
@@ -395,7 +395,7 @@ public class StaticFileCompressServiceImpl implements StaticFileCompressService 
                 if (name.contains(MACOSX) || name.contains(DS_STORE)) {
                     continue;
                 }
-                if (!fileHeader.isDirectory() && name.contains(INDEX_HTML)) {
+                if (!fileHeader.isDirectory() && INDEX_HTML.equals(name)) {
                     notIndex = false;
                     result = getPrefixPathSlash(name);
                 }
@@ -461,7 +461,7 @@ public class StaticFileCompressServiceImpl implements StaticFileCompressService 
                 if (entryName.contains(MACOSX) || entryName.contains(DS_STORE)) {
                     continue;
                 }
-                if (!entry.isDirectory() && entryName.contains(INDEX_HTML)) {
+                if (!entry.isDirectory() && INDEX_HTML.equals(entryName)) {
                     if (inputToByte(in).length <= 0) {
                         throw new CommonException(INDEX_EMPTY_EXCEPTION_CODE);
                     }
@@ -484,7 +484,7 @@ public class StaticFileCompressServiceImpl implements StaticFileCompressService 
                 if (name.contains(MACOSX) || name.contains(DS_STORE)) {
                     continue;
                 }
-                if (!fileHeader.isDirectory() && name.contains(INDEX_HTML)) {
+                if (!fileHeader.isDirectory() && INDEX_HTML.equals(name)) {
                     if (inputToByte(archive.getInputStream(fileHeader)).length <= 0) {
                         throw new CommonException(INDEX_EMPTY_EXCEPTION_CODE);
                     }
