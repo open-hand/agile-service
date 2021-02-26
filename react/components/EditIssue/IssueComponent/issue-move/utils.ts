c9b1739574ceabfd9105f7c3d8229216fce5155f
laincarl <1047972853@qq.com>
2021-02-26 20:53:23 +0800

    [FIX]修复加密id引起的错误

--- react/components/EditIssue/IssueComponent/issue-move/utils.ts
+++ react/components/EditIssue/IssueComponent/issue-move/utils.ts
@@ -1,5 +1,5 @@
 export function split(str:string, gutter:string):string[] {
-  for (let i = 0; i < str.length; i += 1) {
+  for (let i = str.length - 1; i >= 0; i -= 1) {
     if (str[i] === gutter) {
       return [str.slice(0, i), str.slice(i + 1)];
     }
