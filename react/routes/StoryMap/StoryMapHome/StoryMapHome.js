85ec95f2aabef3df675d1973a792008a6eac196b
laincarl <1047972853@qq.com>
2021-02-26 17:31:00 +0800

    [FIX]样式优化

--- react/routes/StoryMap/StoryMapHome/StoryMapHome.js
+++ react/routes/StoryMap/StoryMapHome/StoryMapHome.js
@@ -179,7 +179,7 @@ const StoryMapHome = observer(() => {
       </Header>
       <Breadcrumb />
       <Content style={{
-        padding: 0, borderTop: '1px solid #D8D8D8', overflow: 'hidden',
+        padding: 0, borderTop: '1px solid #D8D8D8', overflow: 'hidden', height: '100%',
       }}
       >
         <Loading loading={loading} />
