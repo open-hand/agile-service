fcc9d63a29abd0251c569668b8b2f5cd23862daa
20615 <wenfei.li@hand-china.com>
2021-03-02 13:39:35 +0800

    [IMP] 添加故事地图卡片上修改史诗名称

---
+++ react/routes/StoryMap/StoryMapHome/components/StoryMapBody/EditEpic.tsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useImperativeHandle, useState } from 'react';
+import { Input } from 'choerodon-ui';
+import { Choerodon } from '@choerodon/boot';
+import { issueApi, epicApi } from '@/api';
+import Card from './Card';
+import StoryMapStore from '../../../../../stores/project/StoryMap/StoryMapStore';
+import clickOutSide from '../../../../../components/CommonComponent/ClickOutSide';
+
+interface Props {
+  epic: {
+    epicName: string
+    issueId: string
+    objectVersionNumber: number
+  }
+  setEditing: (editing: boolean) => void
+  handleClickOutside: () => void
+  editEpicRef: React.MutableRefObject<{
+    handleEditEpic: (value: string) => Promise<void>
+    value: string
+  }>
+}
+
+const EditEpic: React.FC<Props> = ({
+  epic, setEditing, handleClickOutside, editEpicRef,
+}) => {
+  const [value, setValue] = useState(epic.epicName);
+  let canEdit: boolean = true;
+  const handleChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const handleEditEpic = useCallback(async (newValue) => {
+    console.log('提交value:');
+    console.log(newValue);
+
+    if (!canEdit) {
+      return;
+    }
+    canEdit = false;
+    // console.log(e.target.value);
+    if (newValue !== '' && newValue.trim()) {
+      epicApi.checkName(newValue, epic.issueId).then((checkRes: boolean) => {
+        if (checkRes) {
+          Choerodon.prompt('史诗名称重复');
+        } else {
+          const req = {
+            objectVersionNumber: epic.objectVersionNumber,
+            issueId: epic.issueId,
+            epicName: newValue,
+          };
+          issueApi.update(req).then((res) => {
+            setEditing(false);
+            StoryMapStore.afterEditEpicName();
+          }).catch(() => {
+            setEditing(false);
+            setValue(epic.epicName);
+            Choerodon.prompt('编辑史诗失败');
+          });
+        }
+      }).finally(() => {
+        canEdit = true;
+      });
+    } else {
+      canEdit = true;
+      setEditing(false);
+      setValue(epic.epicName);
+    }
+  }, []);
+
+  useImperativeHandle(editEpicRef, () => ({
+    handleEditEpic,
+    value,
+  }));
+
+  return (
+    <div>
+      <Card style={{
+        boxShadow: '0 0 4px -2px rgba(0,0,0,0.50), 0 2px 4px 0 rgba(0,0,0,0.13)',
+        borderRadius: 2,
+        height: 64,
+        margin: '4px 4px 4px 9px',
+        padding: 7,
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+      >
+        <Input autoFocus onPressEnter={handleEditEpic} placeholder="编辑史诗名称" maxLength="22" value={value} onChange={handleChange} />
+      </Card>
+    </div>
+  );
+};
+
+export default clickOutSide(EditEpic);
--- react/routes/StoryMap/StoryMapHome/components/StoryMapBody/EpicCard.js
+++ react/routes/StoryMap/StoryMapHome/components/StoryMapBody/EpicCard.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
-import { Tooltip } from 'choerodon-ui';
+import { Tooltip, Icon } from 'choerodon-ui';
 import { observer } from 'mobx-react';
 import StatusTag from '@/components/StatusTag';
 import EpicDrag from './EpicDrag';
 import StoryMapStore from '../../../../../stores/project/StoryMap/StoryMapStore';
 import Card from './Card';
+import EditEpic from './EditEpic';
 import './EpicCard.less';
-import { CardWidth, CardPaddingLeft } from '../../Constants';
 
 @observer
 class EpicCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+    };
+    this.editEpicRef = createRef();
+  }
+
   handleClick = () => {
     const { epic } = this.props;
     if (epic.issueId) {
@@ -35,6 +43,25 @@ class EpicCard extends Component {
     this.container = ref;
   }
 
+  handleEditEpic(e) {
+    e.stopPropagation();
+    this.setState({
+      editing: true,
+    });
+  }
+
+  setEditing(editing) {
+    this.setState({
+      editing,
+    });
+  }
+
+  handleClickOutside() {
+    if (this.editEpicRef && this.editEpicRef.current?.handleEditEpic) {
+      this.editEpicRef.current?.handleEditEpic(this.editEpicRef.current?.value);
+    }
+  }
+
   render() {
     const {
       epic, subIssueNum, onMouseDown,
@@ -43,32 +70,50 @@ class EpicCard extends Component {
       issueId, epicName, statusVO,
     } = epic;
     const { selectedIssueMap } = StoryMapStore;
+    const { editing } = this.state;
     return (
-      <Card
-        className={`c7nagile-StoryMap-EpicCard minimapCard ${issueId ? '' : 'none'} ${statusVO && statusVO.completed ? 'completedCard' : undefined}  ${selectedIssueMap.has(issueId) ? 'selected' : ''}`}
-        style={{ display: 'flex' }}
-        onClick={this.handleClick}
-        saveRef={this.saveRef}
-        onMouseDown={onMouseDown}
-      >
-        <div className="top">
-          <div className="summary">
-            <Tooltip title={`${epicName || '无史诗'}`} getPopupContainer={() => document.getElementsByClassName('minimap-container-scroll')[0]}>
-              {`${epicName || '无史诗'}`}
-            </Tooltip>
-          </div>
-          <div style={{ marginLeft: 5 }}>{`(${subIssueNum})`}</div>
-        </div>
+      <>
         {
-          epicName && (
-            <div className="status">
-              <StatusTag
-                data={statusVO || {}}
-              />
+        !editing ? (
+          <Card
+            className={`c7nagile-StoryMap-EpicCard minimapCard ${issueId ? '' : 'none'} ${statusVO && statusVO.completed ? 'completedCard' : undefined}  ${selectedIssueMap.has(issueId) ? 'selected' : ''}`}
+            style={{ display: 'flex' }}
+            onClick={this.handleClick}
+            saveRef={this.saveRef}
+            onMouseDown={onMouseDown}
+          >
+            <div className="top">
+              <div className="summary">
+                <Tooltip title={`${epicName || '无史诗'}`} getPopupContainer={() => document.getElementsByClassName('minimap-container-scroll')[0]}>
+                  {`${epicName || '无史诗'}`}
+                </Tooltip>
+              </div>
+              <div style={{ marginLeft: 5 }}>{`(${subIssueNum})`}</div>
+              <div className="c7nagile-StoryMap-EpicCard-editIcon" role="none" onClick={this.handleEditEpic.bind(this)}>
+                <Icon type="mode_edit" />
+              </div>
             </div>
-          )
-        }
-      </Card>
+            {
+              epicName && (
+                <div className="status">
+                  <StatusTag
+                    data={statusVO || {}}
+                  />
+                </div>
+              )
+            }
+          </Card>
+        ) : (
+          <EditEpic
+            epic={epic}
+            // eslint-disable-next-line react/jsx-no-bind
+            setEditing={this.setEditing.bind(this)}
+            handleClickOutside={this.handleClickOutside}
+            editEpicRef={this.editEpicRef}
+          />
+        )
+      }
+      </>
     );
   }
 }
