bc0637d40beae4af0ca4b24a539436ca18e775c8
laincarl <1047972853@qq.com>
2021-03-09 12:59:02 +0800

    [IMP]富文本放在组件库

--- react/components/CKEditor/index.tsx
+++ react/components/CKEditor/index.tsx
@@ -1,126 +1,20 @@
-import React, {
-  useRef, useMemo, useCallback,
-} from 'react';
-import { Button } from 'choerodon-ui/pro';
-import { ButtonColor } from 'choerodon-ui/pro/lib/button/enum';
-import '@choerodon/ckeditor/build/translations/zh-cn';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import { delta2Html } from '@/utils/richText';
-import DecoupledEditor from '@choerodon/ckeditor';
-import UploadAdapterPlugin from './plugins/UploadAdapterPlugin';
-import './index.less';
+import React from 'react';
+import { CKEditor } from '@choerodon/components';
+import { EditorProps as CKEditorProps } from '@choerodon/components/lib/ck-editor';
+import { fileApi } from '@/api';
 
-const prefixCls = 'c7n-ckeditor';
-function isJSON(string: string) {
-  let isDelta = true;
-  try {
-    JSON.parse(string);
-  } catch (error) {
-    isDelta = false;
-  }
-  return isDelta;
+async function handleImageUpload(file:File) {
+  const formData = new FormData();
+  formData.append('file', file);
+  const urls = await fileApi.uploadImage(formData);
+  return urls[0];
 }
-
-interface EditorProps {
-  autoFocus?: boolean
-  toolbar?: boolean
-  footer?: boolean
-  style?: React.CSSProperties
-  value?: string
-  defaultValue?: string
-  onChange?: (value: string) => void
-  onOk?: (value: string) => void
-  okText?: string
-  onCancel?: () => void
-  placeholder?: string
-  disabled?: boolean
-}
-const Editor: React.FC<EditorProps> = ({
-  value: propsValue,
-  defaultValue,
-  onChange,
-  onOk,
-  okText,
-  onCancel,
-  autoFocus,
-  toolbar = true,
-  footer,
-  style,
-  placeholder,
-  disabled,
-}) => {
-  const value = useMemo(() => propsValue ?? defaultValue, [defaultValue, propsValue]);
-  const data = useMemo(() => (value && isJSON(value) ? delta2Html(value) : value), [value]);
-  const editorRef = useRef<any>(null);
-  const handleReady = useCallback((editor) => {
-    if (toolbar) {
-      // Insert the toolbar before the editable area.
-      editor.ui.getEditableElement().parentElement.insertBefore(
-        editor.ui.view.toolbar.element,
-        editor.ui.getEditableElement(),
-      );
-    }
-    editorRef.current = editor;
-    if (autoFocus) {
-      editor.focus();
-    }
-  }, [autoFocus, toolbar]);
-  const handleChange = useCallback((event, editor) => {
-    const newValue = editor.getData();
-    onChange && onChange(newValue);
-  }, [onChange]);
-  const handleSave = useCallback(async () => {
-    if (onOk) {
-      await onOk(editorRef.current?.getData() || '');
-    }
-  }, [onOk]);
-  const handleCancel = useCallback(() => {
-    onCancel && onCancel();
-  }, [onCancel]);
-  const editorConfiguration = useMemo(() => ({
-    language: 'zh-cn',
-    extraPlugins: [UploadAdapterPlugin],
-    placeholder: placeholder ?? '描述',
-    toolbar: {
-      items: [
-        'heading', '|',
-        'fullscreen', '|',
-        'alignment', '|',
-        'bold', 'italic', 'strikethrough', 'underline', 'subscript', 'superscript', '|',
-        'link', '|',
-        'bulletedList', 'numberedList', 'todoList',
-        'fontsize', 'fontColor', 'fontBackgroundColor', '|',
-        'insertTable', '|',
-        'outdent', 'indent', '|',
-        'imageUpload', 'blockQuote', '|',
-        'undo', 'redo',
-      ],
-      shouldNotGroupWhenFull: true,
-    },
-  }), [placeholder]);
-  const editorStyle = useMemo(() => (!style?.height && !style?.minHeight ? { height: 280, ...style } : style), [style]);
-  return (
-    <div className={prefixCls} style={editorStyle}>
-      <CKEditor
-        disabled={disabled}
-        editor={DecoupledEditor}
-        data={data ?? ''}
-        onReady={handleReady}
-        onChange={handleChange}
-        config={editorConfiguration}
-      />
-      {footer && (
-        <div className={`${prefixCls}-footer`}>
-          <Button onClick={handleCancel} color={'blue' as ButtonColor}>
-            取消
-          </Button>
-          <Button onClick={handleSave} color={'blue' as ButtonColor}>
-            {okText ?? '确定'}
-          </Button>
-        </div>
-      )}
-    </div>
-  );
-};
+type EditorProps =Omit<CKEditorProps, 'onImageUpload'>
+const Editor: React.FC<EditorProps> = (props) => (
+  <CKEditor
+    {...props}
+    onImageUpload={handleImageUpload}
+  />
+);
 
 export default Editor;
