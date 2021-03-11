fe4f9e09b18d5fee068fc77c97b169fa608b2ab1
superlee <superleader8@gmail.com>
2021-03-11 19:03:29 +0800

    [ADD] 添加app版本表，app版本和issue关系表，product_version和app_version关系表

---
+++ src/main/java/io/choerodon/agile/infra/dto/AppVersionDTO.java
@@ -0,0 +1,101 @@
+package io.choerodon.agile.infra.dto;
+
+import io.choerodon.mybatis.annotation.ModifyAudit;
+import io.choerodon.mybatis.annotation.VersionAudit;
+import io.choerodon.mybatis.domain.AuditDomain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author superlee
+ * @since 2021-03-09
+ */
+@Table(name = "agile_app_version")
+@ModifyAudit
+@VersionAudit
+public class AppVersionDTO extends AuditDomain {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String groupId;
+
+    private String artifactId;
+
+    private String version;
+
+    private String versionAlias;
+
+    private String serviceCode;
+
+    private Long projectId;
+
+    private Long organizationId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getVersionAlias() {
+        return versionAlias;
+    }
+
+    public void setVersionAlias(String versionAlias) {
+        this.versionAlias = versionAlias;
+    }
+
+    public String getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Long getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Long organizationId) {
+        this.organizationId = organizationId;
+    }
+}
---
+++ src/main/resources/script/db/groovy/agile_service/agile_app_version.groovy
@@ -0,0 +1,36 @@
+package script.db.groovy.agile_service
+
+databaseChangeLog(logicalFilePath: 'script/db/agile_app_version.groovy') {
+    changeSet(id: '2021-03-09-agile-app-version', author: 'kaiwen.li@hand-china.com') {
+        createTable(tableName: "agile_app_version", remarks: '应用版本表') {
+            column(name: 'id', type: 'BIGINT UNSIGNED', autoIncrement: true, remarks: 'id') {
+                constraints(primaryKey: true)
+            }
+            column(name: 'group_id', type: 'VARCHAR(255)', remarks: 'group id') {
+                constraints(nullable: false)
+            }
+            column(name: 'artifact_id', type: 'VARCHAR(255)', remarks: 'artifact id') {
+                constraints(nullable: false)
+            }
+            column(name: 'version', type: 'VARCHAR(255)', remarks: '版本') {
+                constraints(nullable: false)
+            }
+            column(name: 'version_alias', type: 'VARCHAR(255)', remarks: '版本别名')
+            column(name: 'service_code', type: 'VARCHAR(64)', remarks: '服务编码') {
+                constraints(nullable: false)
+            }
+            column(name: 'project_id', type: 'BIGINT UNSIGNED', remarks: 'project id') {
+                constraints(nullable: false)
+            }
+            column(name: 'organization_id', type: 'BIGINT UNSIGNED', remarks: ' ') {
+                constraints(nullable: false)
+            }
+
+            column(name: "object_version_number", type: "BIGINT UNSIGNED", defaultValue: "1")
+            column(name: "created_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "creation_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+            column(name: "last_updated_by", type: "BIGINT UNSIGNED", defaultValue: "0")
+            column(name: "last_update_date", type: "DATETIME", defaultValueComputed: "CURRENT_TIMESTAMP")
+        }
+    }
+}
