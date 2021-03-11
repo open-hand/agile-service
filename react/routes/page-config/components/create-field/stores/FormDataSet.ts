f5c88fc91a7499bef885a5638193165837f443f5
DingZhicheng <zhicheng.ding@hand-china.com>
2021-03-11 11:52:27 +0800

    [FIX]解决文件导入缺失

--- react/routes/page-config/components/create-field/stores/FormDataSet.ts
+++ react/routes/page-config/components/create-field/stores/FormDataSet.ts
@@ -6,6 +6,7 @@ import { RenderProps } from 'choerodon-ui/pro/lib/field/FormField';
 import { DataSetProps } from 'choerodon-ui/pro/lib/data-set/DataSet';
 import { FieldType } from 'choerodon-ui/pro/lib/data-set/enum';
 import { MAX_LENGTH_FIELD_CODE, MAX_LENGTH_FIELD_NAME } from '@/constants/MAX_LENGTH';
+import { MAX_NUMBER_STEP, MAX_NUMBER_VALUE } from '@/constants/MAX_VALUE';
 import { Store } from './useStore';
 
 interface Props {
