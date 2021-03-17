51291c3c1b2af6e3ea9afe368eeb0cfaeb2ea961
laincarl <1047972853@qq.com>
2021-03-17 20:04:44 +0800

    [IMP]优化截图性能

--- react/routes/project-report/report-page/components/export/index.tsx
+++ react/routes/project-report/report-page/components/export/index.tsx
@@ -16,23 +16,29 @@ interface Props {
 const Export: React.FC<Props> = ({ innerRef }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const callbackRef = useRef<(canvas: HTMLCanvasElement[]) => void>();
-  const task = useMemo(() => generateTask('export', () => {
+  const task = useMemo(() => generateTask('export', async () => {
     if (containerRef.current) {
       const container = containerRef.current;
       const blocks = container.getElementsByClassName('c7n-project-report-block');
-      Promise.all(Array.from(blocks).map((element) => html2canvas(element as HTMLElement, {
-        allowTaint: true,
-        useCORS: true,
-        logging: false,
-        height: element.scrollHeight,
-        width: container.scrollWidth,
-        windowHeight: element.scrollHeight,
-        windowWidth: container.scrollWidth,
-      }))).then((canvas) => {
-        setExporting(false);
-        task.reset();
-        callbackRef.current && callbackRef.current(canvas);
-      });
+      const elements = Array.from(blocks);
+      const canvases = [];
+      for (let i = 0; i < elements.length; i += 1) {
+        const element = elements[i];
+        // eslint-disable-next-line no-await-in-loop
+        const canvas = await html2canvas(element as HTMLElement, {
+          allowTaint: true,
+          useCORS: true,
+          logging: false,
+          height: element.scrollHeight,
+          width: container.scrollWidth,
+          windowHeight: element.scrollHeight,
+          windowWidth: container.scrollWidth,
+        });
+        canvases.push(canvas);
+      }
+      setExporting(false);
+      task.reset();
+      callbackRef.current && callbackRef.current(canvases);
     }
   }), []);
   const [exporting, setExporting] = useState(false);
