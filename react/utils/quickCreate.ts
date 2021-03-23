5c576554688506a24428e4c738a60eca072f3db6
DingZhicheng <zhicheng.ding@hand-china.com>
2021-03-23 11:38:23 +0800

    [IMP]整合快速创建问题请求体

---
+++ react/utils/quickCreate.ts
@@ -0,0 +1,92 @@
+import { fieldApi } from '@/api';
+import { isEmpty } from 'lodash';
+import { getProjectId } from './common';
+
+export async function checkCanQuickCreate(typeId: string) {
+  const param = {
+    schemeCode: 'agile_issue',
+    issueTypeId: typeId,
+    pageCode: 'agile_issue_create',
+  };
+  const whiteList = ['summary', 'status', 'reporter', 'issueType', 'priority', 'epicName'];
+  const fields = await fieldApi.getFields(param);
+  if (fields.some((field: any) => !whiteList.includes(field.fieldCode) && field.required && !field.defaultValue)) {
+    return false;
+  }
+  return true;
+}
+interface IQuickCreateDefaultValueParams {
+  issueTypeId: string
+  typeCode: string
+  priorityId?: string
+  projectId?: string
+  programId?: string
+  epicId?: string | number
+  piId?: string
+  summary?: string
+  parentIssueId?: string | number
+  relateIssueId?: string | number
+  sprintId?: string
+  featureVO?: { featureType: string } /** ? */
+  wsjfVO?: any
+  epicName?: string
+  description?: string
+  issueLinkCreateVOList?: Array<any>
+  labelIssueRelVOList?: Array<any>
+  versionIssueRelVOList?: Array<{ versionId: string, relationType: 'fix' | 'influence' }>
+  featureId?: string
+  assigneeId?: string
+  reporterId?: string
+  estimatedStartTime?: string
+  estimatedEndTime?: string
+  [otherPropsName: string]: any
+}
+export function getQuickCreateDefaultObj(defaultValues?: IQuickCreateDefaultValueParams, fieldsMap: Map<string, any> = new Map()): any {
+  if (!defaultValues) {
+    return {};
+  }
+  const { versionIssueRelVOList = [] } = defaultValues;
+  const defaultVersionList: IQuickCreateDefaultValueParams['versionIssueRelVOList'] = [];
+  if (!isEmpty(fieldsMap.get('influenceVersion')?.defaultValue) && !versionIssueRelVOList.some((item = {} as any) => item.relationType === 'influence')) {
+    fieldsMap.get('influenceVersion')?.defaultValue.forEach((item: any) => defaultVersionList.push({
+      versionId: item,
+      relationType: 'influence',
+    }));
+  }
+  if (!isEmpty(fieldsMap.get('fixVersion')?.defaultValue) && !versionIssueRelVOList.some((item = {} as any) => item.relationType === 'fix')) {
+    fieldsMap.get('fixVersion')?.defaultValue.forEach((item: any) => defaultVersionList.push({
+      versionId: item,
+      relationType: 'fix',
+    }));
+  }
+  versionIssueRelVOList.push(...defaultVersionList);
+
+  return {
+    ...defaultValues,
+    summary: defaultValues.summary?.trim(),
+    priorityCode: `priority-${defaultValues.priorityId || 0}`,
+    priorityId: defaultValues.priorityId || 0,
+    projectId: defaultValues.projectId || getProjectId(),
+    programId: defaultValues.projectId || getProjectId(),
+    epicId: defaultValues.epicId || 0,
+    parentIssueId: defaultValues.parentIssueId || 0,
+    relateIssueId: defaultValues.relateIssueId || 0,
+    sprintId: defaultValues.sprintId || fieldsMap.get('sprint')?.defaultValue || 0,
+    epicName: defaultValues.typeCode === 'issue_epic' ? defaultValues.summary?.trim() : undefined,
+    componentIssueRelVOList: fieldsMap.get('component')?.defaultValueObjs || [],
+    description: defaultValues.description || '',
+    issueLinkCreateVOList: [],
+    labelIssueRelVOList: fieldsMap.get('label')?.defaultValueObjs || [],
+    versionIssueRelVOList,
+    // fixVersionIssueRel: fieldsMap.get('fixVersion')?.defaultValue || [],
+    featureId: defaultValues.typeCode === 'story' ? defaultValues.featureId : 0,
+    assigneeId: defaultValues.assigneeId || fieldsMap.get('assignee')?.defaultValue,
+    reporterId: defaultValues.reporterId || fieldsMap.get('reporter')?.defaultValue,
+    estimatedEndTime: defaultValues.estimatedEndTime || fieldsMap.get('estimatedEndTime')?.defaultValue,
+    estimatedStartTime: defaultValues.estimatedStartTime || fieldsMap.get('estimatedStartTime')?.defaultValue,
+    storyPoints: fieldsMap.get('storyPoints')?.defaultValue,
+    remainingTime: fieldsMap.get('remainingTime')?.defaultValue,
+    mainResponsibleId: fieldsMap.get('mainResponsible')?.defaultValue,
+    testResponsibleId: fieldsMap.get('testResponsible')?.defaultValue,
+  };
+}
