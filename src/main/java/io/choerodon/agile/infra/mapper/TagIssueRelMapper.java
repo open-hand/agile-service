2b76d63a3c4c4cf14cfea85d4ac9f47dea1cd825
superlee <superleader8@gmail.com>
2021-03-30 16:35:58 +0800

    [ADD] 添加根据tagId和issueTypeCode查询issueId的方法

--- src/main/java/io/choerodon/agile/infra/mapper/TagIssueRelMapper.java
+++ src/main/java/io/choerodon/agile/infra/mapper/TagIssueRelMapper.java
@@ -2,10 +2,19 @@ package io.choerodon.agile.infra.mapper;
 
 import io.choerodon.agile.infra.dto.TagIssueRelDTO;
 import io.choerodon.mybatis.common.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Set;
 
 /**
  * @author superlee
  * @since 2021-03-25
  */
 public interface TagIssueRelMapper extends BaseMapper<TagIssueRelDTO> {
+
+    Set<Long> selectByTagIdsAndIssueType(@Param("projectId") Long projectId,
+                                         @Param("organizationId") Long organizationId,
+                                         @Param("tagIds") Set<Long> tagIds,
+                                         @Param("issueTypeCode") String issueTypeCode);
+
 }
