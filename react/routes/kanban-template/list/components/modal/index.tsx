93af150e5938a902fce53ce7e0dcdf75f3900b62
laincarl <1047972853@qq.com>
2021-03-30 16:02:39 +0800

    [FIX]修改看板模板

--- react/routes/kanban-template/list/components/modal/index.tsx
+++ react/routes/kanban-template/list/components/modal/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useCallback } from 'react';
 import {
-  Modal, Form, DataSet, TextField, TextArea,
+  Modal, Form, DataSet, TextField,
 } from 'choerodon-ui/pro';
 import { IModalProps } from '@/common/types';
 import MODAL_WIDTH from '@/constants/MODAL_WIDTH';
-import { MAX_LENGTH_KANBAN_NAME, MAX_LENGTH_KANBAN_DESCRIPTION } from '@/constants/MAX_LENGTH';
+import { MAX_LENGTH_KANBAN_NAME } from '@/constants/MAX_LENGTH';
 import { kanbanTemplateApiConfig, IKanbanTemplateEdit, kanbanTemplateApi } from '@/api';
 import { useCreation, usePersistFn } from 'ahooks';
 
