efe60ff52c19768d04bfbe1917ffcaf48c6fe94c
laincarl <1047972853@qq.com>
2021-04-01 11:17:31 +0800

    [FIX]去掉操作

--- react/injects/template-modal/kanban-template/detail/components/status-card/index.tsx
+++ react/injects/template-modal/kanban-template/detail/components/status-card/index.tsx
@@ -1,12 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { Radio } from 'choerodon-ui/pro';
 import { IKanbanTemplateStatus } from '@/api';
 import { observer } from 'mobx-react-lite';
-import { useLockFn } from 'ahooks';
 import StatusTypeTag from '@/components/tag/status-type-tag';
 import styles from './index.less';
-import { Context } from '../..';
 
 const grid = 12;
 interface ColumnProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -20,29 +18,23 @@ const StatusCard: React.FC<ColumnProps> = ({
   columnId,
   data,
   ...otherProps
-}) => {
-  const { store } = useContext(Context);
-  const handleCheckClick = useLockFn(async () => {
-    await store.setStatusComplete(data, !data.templateCompleted);
-  });
-  return (
-    <div
-      className={classNames(styles.status_card, className)}
-      style={{
-        margin: `${grid}px 0 `,
-      }}
-    >
-      <StatusTypeTag
-        mode="tag"
-        code={data.categoryCode}
-        name={data.name}
-      />
-      <br />
-      <div role="none" className={styles.radio} onClick={handleCheckClick}>
-        <Radio checked={data.templateCompleted}>设置已完成</Radio>
-      </div>
+}) => (
+  <div
+    className={classNames(styles.status_card, className)}
+    style={{
+      margin: `${grid}px 0 `,
+    }}
+  >
+    <StatusTypeTag
+      mode="tag"
+      code={data.categoryCode}
+      name={data.name}
+    />
+    <br />
+    <div role="none" className={styles.radio}>
+      <Radio checked={data.templateCompleted}>设置已完成</Radio>
     </div>
-  );
-};
+  </div>
+);
 
 export default observer(StatusCard);
