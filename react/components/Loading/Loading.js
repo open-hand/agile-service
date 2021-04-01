984d4cb1207b9474faf5190f7e60a86d3c900dd4
laincarl <1047972853@qq.com>
2021-04-01 11:21:29 +0800

    [FIX]修复loading不显示

--- react/components/Loading/Loading.js
+++ react/components/Loading/Loading.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import { Spin } from 'choerodon-ui';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Spin } from 'choerodon-ui/pro';
 import './Loading.less';
 
 const Loading = ({
   loading,
 }) => (
-    <Spin spinning={loading} wrapperClassName="c7ntest-Loading">
-      <div style={{ width: '100%', height: '100%' }} />
-    </Spin>
-  );
+  <Spin spinning={loading} wrapperClassName="c7ntest-Loading">
+    <div style={{ width: '100%', height: '100%' }} />
+  </Spin>
+);
 
 Loading.propTypes = {
 
