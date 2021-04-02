78e873e79fe3d6c1f04a68d5e84fd2cb07495df2
laincarl <1047972853@qq.com>
2021-04-02 09:51:39 +0800

    [IMP]添加权限

--- react/routes/Release/index.js
+++ react/routes/Release/index.js
@@ -13,11 +13,11 @@ const ReleaseIndex = ({ match }) => (
     <PermissionRoute
       service={[
         'choerodon.code.project.cooperation.project-version.ps.default',
-        'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.work-list.createversion',
-        'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.worklist.deleteversion',
-        'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.worklist.updateversionstatus',
-        'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.worklist.updateversion',
-        'choerodon.code.project.cooperation.work-list.ps.version.link.program.version',
+        // 'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.work-list.createversion',
+        // 'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.worklist.deleteversion',
+        // 'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.worklist.updateversionstatus',
+        // 'choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.worklist.updateversion',
+        // 'choerodon.code.project.cooperation.work-list.ps.version.link.program.version',
       ]}
       path={`${match.url}`}
       component={ReleaseHome}
