bf9dc8ef60a6b60ac6dbcccb593ea13c8c89c69f
superlee <superleader8@gmail.com>
2021-04-12 19:56:32 +0800

    [ADD] tagVO添加equals和hashCode方法

--- src/main/java/io/choerodon/agile/api/vo/TagVO.java
+++ src/main/java/io/choerodon/agile/api/vo/TagVO.java
@@ -1,15 +1,20 @@
 package io.choerodon.agile.api.vo;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 /**
  * @author superlee
  * @since 2021-03-25
  */
 public class TagVO {
 
+    @NotEmpty(message = "error.tag.app.service.code")
     private String appServiceCode;
-
+    @NotEmpty(message = "error.tag.name")
     private String tagName;
-
+    @NotNull(message = "error.tag.project.id")
     private Long projectId;
 
     private String appServiceName;
@@ -44,5 +49,20 @@ public class TagVO {
 
     public void setAppServiceName(String appServiceName) {
         this.appServiceName = appServiceName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TagVO)) return false;
+        TagVO tagVO = (TagVO) o;
+        return Objects.equals(getAppServiceCode(), tagVO.getAppServiceCode()) &&
+                Objects.equals(getTagName(), tagVO.getTagName()) &&
+                Objects.equals(getProjectId(), tagVO.getProjectId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAppServiceCode(), getTagName(), getProjectId());
     }
 }
