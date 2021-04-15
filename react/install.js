1ba1a8da4dd0916e85368bfb2b140382848e08d8
laincarl <1047972853@qq.com>
2021-04-15 14:52:51 +0800

    [IMP]更改注入

--- react/install.js
+++ react/install.js
@@ -2,3 +2,5 @@ import { set } from '@choerodon/inject';
 import openTemplate from './injects/template-modal';
 
 set('agile:openTemplate', openTemplate);
+set('agile:SelectSprint', () => import('@/components/select/select-sprint'));
+set('agile:SelectVersion', () => import('@/components/select/select-version'));
