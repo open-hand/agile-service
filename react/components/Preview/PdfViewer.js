5be16b6804e04eb784491b9b1597ae08a3d29cd4
王坤奇 <kunqi.wang@hand-china.com>
2021-04-23 14:21:12 +0800

    Revert "Merge branch 'feature-AG-11095' into 'master'"
    This reverts merge request !3078

--- react/components/Preview/PdfViewer.js
+++ react/components/Preview/PdfViewer.js
@@ -1,26 +1,121 @@
-import React, { useState } from 'react';
-import {
-  Document, Page, pdfjs,
-} from 'react-pdf';
+import pdfjs from 'pdfjs-dist/build/pdf';
+import { PDFPageView } from 'pdfjs-dist/web/pdf_viewer';
+import 'pdfjs-dist/web/pdf_viewer.css';
+import React, {
+  useState, useEffect, useRef, useMemo,
+} from 'react';
+import { useUnmountedRef } from 'ahooks';
+import Loading from '../Loading';
+import './PdfViewer.less';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-function PdfViewer({ file }) {
-  const [numPages, setNumPages] = useState(null);
+export const usePdf = ({
+  container,
+  file,
+  scale = 1.0,
+  rotate = 0,
+  page = 1,
+  cMapUrl,
+  cMapPacked,
+  workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.2.228/pdf.worker.js',
+  withCredentials = false,
+}) => {
+  const [pdf, setPdf] = useState();
+  const unmountRef = useUnmountedRef();
+  const rafRef = useRef(null);
+  useEffect(() => {
+    pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+  }, []);
 
-  function onDocumentLoadSuccess({ numPages: p }) {
-    setNumPages(p);
-  }
+  useEffect(() => {
+    const config = { url: file, withCredentials };
+    if (cMapUrl) {
+      config.cMapUrl = cMapUrl;
+      config.cMapPacked = cMapPacked;
+    }
+    pdfjs.getDocument(config).promise.then(setPdf);
+  }, [file, withCredentials]);
+  // 渲染一页
+  const drawPDF = (pageContent) => {
+    if (unmountRef.current) {
+      return null;
+    }
+    const pdfPageView = new PDFPageView({
+      container: container.current,
+      id: page,
+      scale,
+      defaultViewport: pageContent.getViewport({ scale }),
+    });
+    pdfPageView.setPdfPage(pageContent);
+    return pdfPageView.draw();
+  };
+
+  const loading = useMemo(() => !pdf, [pdf]);
+  // eslint-disable-next-line no-underscore-dangle
+  const numPages = useMemo(() => (pdf ? pdf._pdfInfo.numPages : null), [pdf]);
+
+  useEffect(() => {
+    if (pdf) {
+      // eslint-disable-next-line no-param-reassign
+      container.current.innerHTML = '';
+      // eslint-disable-next-line no-inner-declarations
+      function renderPdf(curTotal, curIndex) {
+        if (curTotal <= 0) {
+          return false;
+        }
+        rafRef.current = window.requestAnimationFrame(() => {
+          pdf.getPage(curIndex).then((p) => drawPDF(p));
+          renderPdf(curTotal - 1, curIndex + 1);
+        });
+        return null;
+      }
+      renderPdf(numPages, 1);
+    }
+    return () => {
+      window.cancelAnimationFrame(rafRef.current);
+    };
+  }, [pdf, scale, rotate]);
+
+  return [loading, numPages];
+};
+
+const Pdf = ({
+  file,
+  onDocumentComplete,
+  scale,
+  rotate,
+  cMapUrl,
+  cMapPacked,
+  workerSrc,
+  withCredentials,
+}) => {
+  const container = useRef(null);
+  const [page, setPage] = useState(1);
+  const [loading, numPages] = usePdf({
+    container,
+    file,
+    page,
+    scale,
+    rotate,
+    cMapUrl,
+    cMapPacked,
+    workerSrc,
+    withCredentials,
+  });
+
+  useEffect(() => {
+    onDocumentComplete(numPages);
+  }, [numPages]);
 
   return (
-    <Document
-      file={file}
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
-      {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
+    <div className="c7n-pdf-viewer">
+      <div className="c7n-pdf-viewer-content" ref={container} />
+      {loading && <div><Loading /></div>}
+    </div>
   );
-}
+};
 
-export default PdfViewer;
+Pdf.defaultProps = {
+  onDocumentComplete: () => { },
+};
+
+export default Pdf;
