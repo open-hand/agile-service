e3f8b9dc7bfbdcd74bb49c83bece239590694c62
laincarl <1047972853@qq.com>
2021-04-23 10:40:51 +0800

    [FIX]看板切换改成pro组件

---
+++ react/hooks/useSelectFooter.tsx
@@ -0,0 +1,14 @@
+import { usePersistFn } from 'ahooks';
+import { Select } from 'choerodon-ui/pro';
+import React, { RefObject } from 'react';
+
+export default function useSelectFooter(ref: RefObject<Select>, footer: React.ReactNode) {
+  const menu = React.Children.toArray(ref.current?.getPopupContent()) ?? [];
+  const popupContent = usePersistFn(() => [
+    ...menu,
+    footer,
+  ]);
+  return {
+    popupContent,
+  };
+}
