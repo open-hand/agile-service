fff26e4a8756f7611e547f23644f16dbd73cccac
DingZhicheng <zhicheng.ding@hand-china.com>
2021-04-26 11:41:50 +0800

    [FIX]修复新主题下版本列表样式错乱

--- react/routes/version-list/index.js
+++ react/routes/version-list/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import {
-  asyncRouter, PageWrap, PageTab,
+  asyncRouter, PageWrap, PageTab, Permission,
 } from '@choerodon/boot';
 import LINK_URL from '@/constants/LINK_URL';
 
@@ -9,12 +9,18 @@ const Release = withRouter(asyncRouter(() => import('../Release')));
 const Publish = withRouter(asyncRouter(() => (import('../publish-version'))));
 
 const WorkList = ({ match }) => (
-  <PageWrap
-    noHeader={[]}
-  >
-    <PageTab title="规划版本" route={LINK_URL.versionRelease} tabKey="choerodon.code.project.cooperation.version-list.ps.release" component={Release} />
-    <PageTab title="发布版本" route={LINK_URL.versionPublish} tabKey="choerodon.code.project.cooperation.version-list.ps.publish" component={Publish} />
+  <Permission service={['choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.work-list.createversion']}>
+    {
+      (permission, loading) => (
+        <PageWrap
+          noHeader={!loading && permission ? [] : ['choerodon.code.project.cooperation.version-list.ps.release']}
+        >
+          <PageTab title="规划版本" route={LINK_URL.versionRelease} tabKey="choerodon.code.project.cooperation.version-list.ps.release" component={Release} />
+          <PageTab title="发布版本" route={LINK_URL.versionPublish} tabKey="choerodon.code.project.cooperation.version-list.ps.publish" component={Publish} />
 
-  </PageWrap>
+        </PageWrap>
+      )
+    }
+  </Permission>
 );
 export default WorkList;
