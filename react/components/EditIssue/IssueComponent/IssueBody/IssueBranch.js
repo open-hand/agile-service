eacfbfceedee9bad24e74659a770b18afabb15ed
laincarl <1047972853@qq.com>
2021-04-27 13:48:42 +0800

    [FIX]离开开发tab后恢复宽度

--- react/components/EditIssue/IssueComponent/IssueBody/IssueBranch.js
+++ react/components/EditIssue/IssueComponent/IssueBody/IssueBranch.js
@@ -11,6 +11,7 @@ const IssueBranch = observer(({
   store, disabled, projectId, reloadIssue, otherProject, outside, programId, applyType, issueId,
 }) => {
   const commitsRef = useRef();
+  const originWidthRef = useRef();
   const mergeRequestRef = useRef();
   const { resizeRef } = useDetailContainerContext();
   const refresh = useCallback(() => {
@@ -20,14 +21,22 @@ const IssueBranch = observer(({
   useEffect(() => {
     store.setRefreshBranch(refresh);
   }, [refresh, store]);
+  const { tab } = store;
+  const isDevelopTab = tab === 'development';
   useEffect(() => {
-    if (resizeRef.current) {
+    if (isDevelopTab && resizeRef.current) {
       const maxWidth = window.innerWidth * 0.6;
       if (resizeRef.current.resizable.clientWidth < maxWidth) {
+        originWidthRef.current = resizeRef.current.resizable.clientWidth;
         resizeRef.current.setWidth(maxWidth);
       }
     }
-  }, [resizeRef]);
+    return () => {
+      if (isDevelopTab && resizeRef.current) {
+        resizeRef.current.setWidth(originWidthRef.current);
+      }
+    };
+  }, [resizeRef, isDevelopTab]);
   return (
     <div id="branch">
       <div className="c7n-title-wrapper">
