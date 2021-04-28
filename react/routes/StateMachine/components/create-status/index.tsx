8da74ee04ff95c065aa911c8e43a8838dbb17360
20615 <wenfei.li@hand-china.com>
2021-04-28 09:45:05 +0800

    [IMP] 修复创建状态弹框史诗类型显示id的问题

--- react/routes/StateMachine/components/create-status/index.tsx
+++ react/routes/StateMachine/components/create-status/index.tsx
@@ -31,7 +31,7 @@ const CreateStatus: React.FC<Props> = ({
   modalRef.current = modal;
   const isOrganization = getIsOrganization();
   const [type, setType] = useState<IStatus['valueCode'] | null>(null);
-  const { data: issueTypes } = useIssueTypes({ hasTemplate: true, excludeTypes: ['feature', 'issue_epic', 'issue_auto_test', 'issue_test'] });
+  const { data: issueTypes } = useIssueTypes({ hasTemplate: true, excludeTypes: ['feature', 'issue_auto_test', 'issue_test', ...(isOrganization ? ['issue_epic'] : [])] });
   // 记录哪些类型下已经有同名状态
   const [hasStatusIssueTypes, setHasStatusIssueTypes] = useState<IIssueType[]>([]);
   const hasStatusIssueTypesRef = useRef<IIssueType[]>([]);
