98e43ec6c10c7ad2b8d7486ceaedbe6f226e4cf6
20615 <wenfei.li@hand-china.com>
2021-05-07 13:40:47 +0800

    [IMP] 修改敏捷服务时间的显示方式

--- react/components/IssueDetail/components/fields/update-date/index.tsx
+++ react/components/IssueDetail/components/fields/update-date/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useDetailContext } from '@/components/IssueDetail/context';
-import DatetimeAgo from '@/components/CommonComponent/DatetimeAgo';
 import Field from '../field';
 
 const CreateDate: React.FC = () => {
@@ -10,11 +9,7 @@ const CreateDate: React.FC = () => {
   return (
     <Field label="更新时间">
       <div style={{ padding: '0 0.05rem 0 0.05rem' }}>
-        {lastUpdateDate ? (
-          <DatetimeAgo
-            date={lastUpdateDate}
-          />
-        ) : '-'}
+        {lastUpdateDate || '-'}
       </div>
     </Field>
   );
