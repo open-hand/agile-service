c6323605aef45c8aa712ba9bc1f9aa3261dda361
DingZhicheng <zhicheng.ding@hand-china.com>
2021-05-19 15:51:18 +0800

    [IMP]发布版本去除导出预览&对比结果增加tag列

--- react/routes/version-list/index.js
+++ react/routes/version-list/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import {
-  withRouter, Route, Switch, RouteChildrenProps,
+  withRouter,
 } from 'react-router-dom';
-
 import {
   asyncRouter, PageWrap, PageTab, Permission,
-  nomatch,
 } from '@choerodon/boot';
 import LINK_URL from '@/constants/LINK_URL';
 
 const Release = withRouter(asyncRouter(() => import('../Release')));
 const Publish = withRouter(asyncRouter(() => (import('../publish-version'))));
-const PublishVersionPreview = React.lazy(() => import('../publish-version/PublishVersionPreview'));
 const VersionList = ({ match }) => (
   <Permission service={['choerodon.code.project.cooperation.work-list.ps.choerodon.code.cooperate.work-list.createversion']}>
     {
@@ -28,14 +25,5 @@ const VersionList = ({ match }) => (
     }
   </Permission>
 );
-const Index = ({ match }) => (
-  <Switch>
-    <Route exact path={match?.url} component={VersionList} />
-    <Route exact path={LINK_URL.versionRelease} component={VersionList} />
-    <Route exact path={LINK_URL.versionPublish} component={VersionList} />
-    <Route exact path={`${match?.url}/publish/preview/:id`} component={PublishVersionPreview} />
-    <Route exact path={`${match?.url}/preview/:id`} component={PublishVersionPreview} />
-    <Route path="*" component={nomatch} />
-  </Switch>
-);
-export default Index;
+
+export default VersionList;
