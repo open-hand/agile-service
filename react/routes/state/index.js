90d5128fd49500a28ba37c86603205c6a46d8012
laincarl <1047972853@qq.com>
2021-06-01 10:43:01 +0800

    [FIX]修复状态机模块切换会重新mount

--- react/routes/state/index.js
+++ react/routes/state/index.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { PageWrap, PageTab, asyncRouter } from '@choerodon/boot';
 import './index.less';
+import { usePersistFn } from 'ahooks';
 
 const KanbanTemplate = asyncRouter(() => import('../kanban-template/list'));
 const StateList = asyncRouter(() => import('./state-list'));
 const StatusMachineTemplate = asyncRouter(() => import('../statusMachine-template'));
 const TemplateManage = () => {
   const [activeKey, setActiveKey] = useState('status_change');
+  const component = usePersistFn(() => (
+    <StatusMachineTemplate
+      activeKey={activeKey}
+      setActiveKey={setActiveKey}
+    />
+  ));
   return (
     <PageWrap
       noHeader={activeKey === 'custom' ? ['choerodon.code.organization.setting.issue.states.ps.stateMachine'] : []}
@@ -22,12 +29,7 @@ const TemplateManage = () => {
         title="状态机模板"
         route="/agile/states/stateMachine"
         tabKey="choerodon.code.organization.setting.issue.states.ps.stateMachine"
-        component={() => (
-          <StatusMachineTemplate
-            activeKey={activeKey}
-            setActiveKey={setActiveKey}
-          />
-        )}
+        component={component}
       />
       <PageTab
         title="看板模板"
