d987bf4b5c66803c8cb59433dcd7dd78373e70cd
laincarl <1047972853@qq.com>
2021-06-01 14:42:37 +0800

    [FIX]修复某些字段选择之后没有保存

--- react/stores/project/issue/IssueStore.js
+++ react/stores/project/issue/IssueStore.js
@@ -2,55 +2,10 @@
 import {
   observable, action, computed, toJS,
 } from 'mobx';
-import {
-  debounce, isEmpty, isEqual, pick, includes,
-} from 'lodash';
+import { includes } from 'lodash';
 import { isInProgram } from '@/utils/program';
 import { has } from '@choerodon/inject';
 
-export const isFilterSame = (obj, obj2) => {
-  // 过滤掉 [] null '' 那些不起作用的属性
-  const keys1 = Object.keys(obj).filter((k) => !isEmpty(obj[k]));
-  const keys2 = Object.keys(obj2).filter((k) => !isEmpty(obj2[k]));
-  return isEqual(pick(obj, keys1), pick(obj2, keys2));
-};
-/**
- * 对象扁平化 {a:{b:'v'}}  = >  {b:'v'}
- *
- * @param {*} object
- */
-export function flattenObject(object) {
-  const result = {};
-  for (const [key, value] of Object.entries(object || {})) {
-    if (Object.prototype.toString.call(value) === '[object Object]') {
-      Object.assign(result, flattenObject(value));
-    } else {
-      result[key] = value;
-    }
-  }
-  const {
-    date = [],
-    date_hms = [],
-    number = [],
-    option = [],
-    string = [],
-    text = [],
-  } = result;
-  [...date, ...date_hms].forEach((d) => {
-    result[d.fieldId] = { isCustom: true, value: [d.startDate, d.endDate] };
-  });
-  [...number, ...option, ...string, ...text].forEach((d) => {
-    result[d.fieldId] = { isCustom: true, value: d.value };
-  });
-
-  delete result.date;
-  delete result.date_hms;
-  delete result.number;
-  delete result.option;
-  delete result.string;
-  delete result.text;
-  return result;
-}
 export function getSystemFields(excludeCodes = []) {
   const systemFields = [{
     code: 'issueIds',
