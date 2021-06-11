309652aadd09f096b0b5e4e90d3e9879dd9ab08e
zhaotianxin <ztxemail@163.com>
2021-06-11 10:34:52 +0800

    [IMP] 添加关联问题联动的逻辑实现

---
+++ src/main/java/io/choerodon/agile/infra/dto/LinkIssueStatusLinkageDTO.java
@@ -0,0 +1,115 @@
+package io.choerodon.agile.infra.dto;
+
+import io.choerodon.mybatis.annotation.ModifyAudit;
+import io.choerodon.mybatis.annotation.VersionAudit;
+import io.choerodon.mybatis.domain.AuditDomain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author zhaotianxin
+ * @date 2021-06-09 10:59
+ */
+@Table(name = "fd_link_issue_status_linkage")
+@ModifyAudit
+@VersionAudit
+public class LinkIssueStatusLinkageDTO extends AuditDomain {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long issueTypeId;
+
+    private Long statusId;
+
+    private Long linkTypeId;
+
+    private Long linkIssueTypeId;
+
+    private Long linkIssueStatusId;
+
+    private Long projectId;
+
+    private Long organizationId;
+
+    public LinkIssueStatusLinkageDTO() {
+    }
+
+    public LinkIssueStatusLinkageDTO(Long projectId, Long organizationId) {
+        this.projectId = projectId;
+        this.organizationId = organizationId;
+    }
+
+    public LinkIssueStatusLinkageDTO(Long issueTypeId, Long statusId, Long projectId, Long organizationId) {
+        this.issueTypeId = issueTypeId;
+        this.statusId = statusId;
+        this.projectId = projectId;
+        this.organizationId = organizationId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIssueTypeId() {
+        return issueTypeId;
+    }
+
+    public void setIssueTypeId(Long issueTypeId) {
+        this.issueTypeId = issueTypeId;
+    }
+
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public Long getLinkTypeId() {
+        return linkTypeId;
+    }
+
+    public void setLinkTypeId(Long linkTypeId) {
+        this.linkTypeId = linkTypeId;
+    }
+
+    public Long getLinkIssueTypeId() {
+        return linkIssueTypeId;
+    }
+
+    public void setLinkIssueTypeId(Long linkIssueTypeId) {
+        this.linkIssueTypeId = linkIssueTypeId;
+    }
+
+    public Long getLinkIssueStatusId() {
+        return linkIssueStatusId;
+    }
+
+    public void setLinkIssueStatusId(Long linkIssueStatusId) {
+        this.linkIssueStatusId = linkIssueStatusId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Long getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Long organizationId) {
+        this.organizationId = organizationId;
+    }
+}
---
+++ src/main/java/io/choerodon/agile/infra/mapper/LinkIssueStatusLinkageMapper.java
@@ -0,0 +1,11 @@
+package io.choerodon.agile.infra.mapper;
+
+import io.choerodon.agile.infra.dto.LinkIssueStatusLinkageDTO;
+import io.choerodon.mybatis.common.BaseMapper;
+
+/**
+ * @author zhaotianxin
+ * @date 2021-06-09 11:03
+ */
+public interface LinkIssueStatusLinkageMapper extends BaseMapper<LinkIssueStatusLinkageDTO> {
+}
