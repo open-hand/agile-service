2cfcc5c7a59dc955ffec7c9ba72da89080704f40
DingZhicheng <zhicheng.ding@hand-china.com>
2021-07-05 11:29:09 +0800

    [ADD]自定义报表增加加载数据方法导出

--- react/install.js
+++ react/install.js
@@ -1,4 +1,5 @@
 import { set } from '@choerodon/inject';
+import { loadCustomReportData } from '@/injects/custom-report/inject';
 import openTemplate from './injects/template-modal';
 
 set('agile:openTemplate', openTemplate);
@@ -6,3 +7,4 @@ set('agile:SelectSprint', () => import('@/components/select/select-sprint'));
 set('agile:SelectVersion', () => import('@/components/select/select-version'));
 set('agile:PersonalWorkload', () => import('@/injects/personal-workload'));
 set('agile:AgileChartHeaderButtons', () => import('@/injects/custom-report/header-buttons'));
+set('agile:AgileCustomChartLoadData', loadCustomReportData);
