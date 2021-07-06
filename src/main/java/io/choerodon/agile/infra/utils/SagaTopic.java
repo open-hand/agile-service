02e5e2d98c201b25b572182b7082681b791905b7
superlee <superleader8@gmail.com>
2021-07-06 09:59:44 +0800

    [DEL] 移除sagaTaskCode

--- src/main/java/io/choerodon/agile/infra/utils/SagaTopic.java
+++ src/main/java/io/choerodon/agile/infra/utils/SagaTopic.java
@@ -29,20 +29,6 @@ public final class SagaTopic {
         public static final String TASK_PROJECT_UPDATE = "agile-update-project";
 
         /**
-         * devops删除tag
-         */
-        public static final String GIT_TAG_DELETE = "devops-git-tag-delete";
-
-        public static final String TASK_GIT_TAG_DELETE = "agile-git-tag-delete";
-
-        /**
-         * devops移除issue和branch关系
-         */
-        public static final String DEVOPS_REMOVE_ISSUE_BRANCH_REL = "devops-branch-issue-delete";
-
-        public static final String TASK_DEVOPS_REMOVE_ISSUE_BRANCH_REL = "agile-remove-issue-tag-rel";
-
-        /**
          * 分支合并
          */
         public static final String BRANCH_MERGE_REQUEST_PASS = "devops-merge-request-pass";
