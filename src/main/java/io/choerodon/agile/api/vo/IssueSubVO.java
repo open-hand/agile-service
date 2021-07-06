ec6b8eccb1e3fd373b4c99cd9b8bf82eca68481d
superlee <superleader8@gmail.com>
2021-07-06 15:45:30 +0800

    [ADD] 问题类型转换为子任务，返回值添加estimatedStartTime和estimatedEndTime

--- src/main/java/io/choerodon/agile/api/vo/IssueSubVO.java
+++ src/main/java/io/choerodon/agile/api/vo/IssueSubVO.java
@@ -142,6 +142,26 @@ public class IssueSubVO {
 
     private List<Long> influenceIssueIds;
 
+    private Date estimatedStartTime;
+
+    private Date estimatedEndTime;
+
+    public Date getEstimatedStartTime() {
+        return estimatedStartTime;
+    }
+
+    public void setEstimatedStartTime(Date estimatedStartTime) {
+        this.estimatedStartTime = estimatedStartTime;
+    }
+
+    public Date getEstimatedEndTime() {
+        return estimatedEndTime;
+    }
+
+    public void setEstimatedEndTime(Date estimatedEndTime) {
+        this.estimatedEndTime = estimatedEndTime;
+    }
+
     public List<Long> getInfluenceIssueIds() {
         return influenceIssueIds;
     }
