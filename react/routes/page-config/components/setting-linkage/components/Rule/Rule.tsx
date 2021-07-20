13b0d92b54f3212c3d9071ed3d2baeb9b2e30232
20615 <wenfei.li@hand-china.com>
2021-07-20 11:30:21 +0800

    [IMP] 添加可见选项相关逻辑

--- react/routes/page-config/components/setting-linkage/components/Rule/Rule.tsx
+++ react/routes/page-config/components/setting-linkage/components/Rule/Rule.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form, Select, CheckBox } from 'choerodon-ui/pro';
 import { observer } from 'mobx-react-lite';
 import Record from 'choerodon-ui/pro/lib/data-set/Record';
@@ -11,15 +11,33 @@ interface Props {
   record: Record
 }
 
-const Rule: React.FC<Props> = ({ record }) => (
-  <div className={styles.rule}>
-    <Form record={record}>
-      {record.get('chosenField')?.fieldType && includes(selectTypes, record.get('chosenField')?.fieldType) ? renderFieldRelSelect({ field: record.get('chosenField') }) : null}
-      {renderDefaultValue({ field: record.get('chosenField'), fieldOptions: record.get('fieldRelOptionList') })}
-      <CheckBox name="hidden" />
-      <CheckBox name="required" />
-    </Form>
-  </div>
-);
+const Rule: React.FC<Props> = ({ record }) => {
+  useEffect(() => {
+    const currentFieldRelOptionList = [...(record?.get('fieldRelOptionList') || [])];
+    console.log('请求可见选项', currentFieldRelOptionList);
+    if (currentFieldRelOptionList.find((item: { value: string, meaning?: string}) => !item.meaning)) {
+      const res: {id: string, name: string }[] = []; // 请求结果
+      currentFieldRelOptionList.map((item) => {
+        if (item.value && item.meaning) {
+          return item;
+        }
+        return { value: item.value, meaning: res?.find((option) => option.id === item.value)?.name };
+      });
+      record.set('fieldRelOptionList', currentFieldRelOptionList);
+    }
+  }, [record]);
+
+  console.log(record?.get('fieldRelOptionList'));
+  return (
+    <div className={styles.rule}>
+      <Form record={record}>
+        {record.get('chosenField')?.fieldType && includes(selectTypes, record.get('chosenField')?.fieldType) ? renderFieldRelSelect({ field: record.get('chosenField') }) : null}
+        {renderDefaultValue({ field: record.get('chosenField'), fieldOptions: record.get('fieldRelOptionList') })}
+        <CheckBox name="hidden" />
+        <CheckBox name="required" />
+      </Form>
+    </div>
+  );
+};
 
 export default observer(Rule);
