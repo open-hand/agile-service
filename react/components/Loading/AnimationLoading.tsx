c48c6d5ded950e36bd60b0ac4aab99e2977fe999
DingZhicheng <zhicheng.ding@hand-china.com>
2021-07-28 09:13:44 +0800

    [IMP]吐泡泡loading更改子元素显示方式

--- react/components/Loading/AnimationLoading.tsx
+++ react/components/Loading/AnimationLoading.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { AnimationLoading as Loading } from '@choerodon/components';
 import classNames from 'classnames';
 import './AnimationLoading.less';
@@ -14,13 +14,21 @@ const AnimationLoading: React.FC<IAnimationLoadingProps> = ({
   loading, style, contentStyle, className, children, contentClassName,
 }) => {
   const prefixCls = 'c7n-agile-animation-loading';
-  return (
+  //   const ref = useRef<any>(null);
+  //   useEffect(() => {
+  //     if (!loading && ref.current) {
+  //     //   ReactDom.render(ref.current.children, containerRef.current);
+  //       console.log('ref...', ref.current.children);
+  //     }
+  //   }, [loading]);
+  return loading ? (
     <div className={classNames(prefixCls, className)} style={style}>
       <Loading display={loading} />
+      {/* {ref.curren} */}
       <div style={{ visibility: loading ? 'hidden' : 'visible', ...contentStyle }} className={contentClassName}>
         {children}
       </div>
     </div>
-  );
+  ) : <>{children}</>;
 };
 export default AnimationLoading;
