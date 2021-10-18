08c1e16ff14763c31f16db91bc438d7011f438da
DingZhicheng <zhicheng.ding@hand-china.com>
2021-10-18 19:55:03 +0800

    [IMP]更改甘特图查询接口传参

--- react/api/Gantt.ts
+++ react/api/Gantt.ts
@@ -58,12 +58,12 @@ class GanttApi {
     });
   }
 
-  loadInfluenceIssues(dimension: string, issueIds: string[], displayFieldCodes: string[]) {
+  loadInfluenceIssues(dimension: string, issueIds: string[], displayFields: { code: string, projectId?: string }[]) {
     return axios({
       method: 'post',
       url: `${this.prefix}/gantt/list_by_ids`,
       params: { dimension },
-      data: { issueIds, displayFieldCodes },
+      data: { issueIds, displayFields },
     });
   }
 
