9915d20057d15c1014f3331f1b2a68f0831750f4
superlee <superleader8@gmail.com>
2021-11-02 19:43:58 +0800

    [IMP] 组织层甘特图项目接口只返回创建成功的额项目

--- src/main/java/io/choerodon/agile/app/service/impl/OrganizationGanttChartServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/OrganizationGanttChartServiceImpl.java
@@ -78,7 +78,7 @@ public class OrganizationGanttChartServiceImpl implements OrganizationGanttChart
     public List<ProjectVO> listAgileProjects(Long organizationId, String name, String code, String param) {
         Page<ProjectVO> projectPage =
                 baseFeignClient
-                        .pagedQueryProjects(organizationId, 1, 0, name, code, true, param)
+                        .pagedQueryProjects(organizationId, 1, 0, name, code, true, true, param)
                         .getBody();
         List<ProjectVO> projects = projectPage.getContent();
         projects =
@@ -88,7 +88,9 @@ public class OrganizationGanttChartServiceImpl implements OrganizationGanttChart
                             Set<String> categories =
                                     x.getCategories().stream().map(ProjectCategoryDTO::getCode).collect(Collectors.toSet());
                             return categories.contains(ProjectCategory.MODULE_AGILE);
-                        }).collect(Collectors.toList());
+                        })
+                        .filter(x -> "success".equalsIgnoreCase(x.getProjectStatus()))
+                        .collect(Collectors.toList());
         return projects;
     }
 
