5c6c67061a918ef45863f88f756a3f9a43838d28
20615 <wenfei.li@hand-china.com>
2021-11-10 10:46:08 +0800

    [IMP] 修复工时日历总计实际工时出现多位小数点的问题 & 修复当天书较少时查看更多按钮不居中的问题 & 修复组织层页面一直闪动的问题

--- react/routes/working-hours/index.tsx
+++ react/routes/working-hours/index.tsx
@@ -4,6 +4,7 @@ import {
   PageWrap, PageTab,
 } from '@choerodon/boot';
 import { useTabActiveKey } from '@choerodon/components';
+import styles from './index.less';
 
 const WorkingHoursLog = withRouter(React.lazy(() => (import('./working-hours-log'))));
 const WorkingHoursCalendar = withRouter(React.lazy(() => (import('./working-hours-calendar'))));
@@ -13,6 +14,7 @@ const WorkingHours = () => {
     <PageWrap
       noHeader={[]}
       onChange={setActiveKey}
+      className={styles.workingHoursTab}
     >
       <PageTab title="工时日历" route="/agile/working-hours/calendar" tabKey="calendar" component={WorkingHoursCalendar} />
       <PageTab title="工时日志" route="/agile/working-hours/log" tabKey="log" component={WorkingHoursLog} />
