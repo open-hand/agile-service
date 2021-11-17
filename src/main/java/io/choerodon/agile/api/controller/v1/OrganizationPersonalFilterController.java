42f7a783cec37f0cbde948273186390ec442e1ce
huaxin <2502353032@qq.com>
2021-11-17 15:12:59 +0800

    [IMP] 个人筛选表增加organizationId和filterTypeCode字段

--- src/main/java/io/choerodon/agile/api/controller/v1/OrganizationPersonalFilterController.java
+++ src/main/java/io/choerodon/agile/api/controller/v1/OrganizationPersonalFilterController.java
@@ -2,14 +2,12 @@ package io.choerodon.agile.api.controller.v1;
 
 import io.choerodon.agile.api.vo.PersonalFilterVO;
 import io.choerodon.agile.app.service.PersonalFilterService;
-
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.swagger.annotation.Permission;
-import io.choerodon.core.exception.CommonException;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.hzero.starter.keyencrypt.core.Encrypt;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,102 +17,103 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * @author shinan.chen
- * @since 2019/2/25
+ * @author huaxin.deng@hand-china.com 2021-11-16 16:18:57
  */
 @RestController
-@RequestMapping(value = "/v1/projects/{project_id}/personal_filter")
-public class PersonalFilterController {
+@RequestMapping(value = "/v1/organizations/{organization_id}/personal_filter")
+public class OrganizationPersonalFilterController {
     @Autowired
     private PersonalFilterService personalFilterService;
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("创建我的筛选")
+    @ApiOperation("组织层创建我的筛选")
     @PostMapping
-    public ResponseEntity<PersonalFilterVO> create(@ApiParam(value = "项目id", required = true)
-                                                   @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<PersonalFilterVO> create(@ApiParam(value = "组织id", required = true)
+                                                   @PathVariable(name = "organization_id") Long organizationId,
                                                    @ApiParam(value = "personal filter object", required = true)
                                                    @RequestBody @Encrypt PersonalFilterVO personalFilterVO) {
-        return Optional.ofNullable(personalFilterService.create(0L, projectId, personalFilterVO))
+        return Optional.ofNullable(personalFilterService.create(organizationId, 0L, personalFilterVO))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
                 .orElseThrow(() -> new CommonException("error.personalFilter.create"));
     }
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("修改我的筛选")
+    @ApiOperation("组织层修改我的筛选")
     @PutMapping(value = "/{filterId}")
-    public ResponseEntity<PersonalFilterVO> update(@ApiParam(value = "项目id", required = true)
-                                                   @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<PersonalFilterVO> update(@ApiParam(value = "组织id", required = true)
+                                                   @PathVariable(name = "organization_id") Long organizationId,
                                                    @ApiParam(value = "filter id", required = true)
-                                                   @PathVariable  @Encrypt Long filterId,
+                                                   @PathVariable @Encrypt Long filterId,
                                                    @ApiParam(value = "personal filter object", required = true)
-                                                   @RequestBody  @Encrypt PersonalFilterVO personalFilterVO) {
-        return Optional.ofNullable(personalFilterService.update(0L, projectId, filterId, personalFilterVO))
+                                                   @RequestBody @Encrypt PersonalFilterVO personalFilterVO) {
+        return Optional.ofNullable(personalFilterService.update(organizationId, 0L, filterId, personalFilterVO))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.CREATED))
                 .orElseThrow(() -> new CommonException("error.personalFilter.create"));
     }
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("删除我的筛选")
+    @ApiOperation("组织层删除我的筛选")
     @DeleteMapping(value = "/{filterId}")
-    public ResponseEntity<PersonalFilterVO> deleteById(@ApiParam(value = "项目id", required = true)
-                                                       @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<PersonalFilterVO> deleteById(@ApiParam(value = "组织id", required = true)
+                                                       @PathVariable(name = "organization_id") Long organizationId,
                                                        @ApiParam(value = "filter id", required = true)
                                                        @PathVariable  @Encrypt Long filterId) {
-        personalFilterService.deleteById(0L, projectId, filterId);
+        personalFilterService.deleteById(organizationId, 0L, filterId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("查询我的筛选列表")
+    @ApiOperation("组织层查询我的筛选列表")
     @GetMapping(value = "/query_all/{userId}")
-    public ResponseEntity<List<PersonalFilterVO>> listByProjectId(@ApiParam(value = "项目id", required = true)
-                                                                  @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<List<PersonalFilterVO>> listByProjectId(@ApiParam(value = "组织id", required = true)
+                                                                  @PathVariable(name = "organization_id") Long organizationId,
                                                                   @ApiParam(value = "用户id", required = true)
                                                                   @PathVariable @Encrypt Long userId,
+                                                                  @ApiParam(value = "查询参数")
                                                                   @RequestParam(name = "searchStr", required = false) String searchStr,
+                                                                  @ApiParam(value = "类型code", required = true)
                                                                   @RequestParam(name = "filterTypeCode") String filterTypeCode) {
-        return Optional.ofNullable(personalFilterService.listByUserId(0L, projectId, userId, searchStr, filterTypeCode))
+        return Optional.ofNullable(personalFilterService.listByUserId(organizationId, 0L, userId, searchStr, filterTypeCode))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.personalFilter.list"));
     }
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("根据id查询我的筛选")
+    @ApiOperation("组织层根据id查询我的筛选")
     @GetMapping(value = "/{filterId}")
-    public ResponseEntity<PersonalFilterVO> queryById(@ApiParam(value = "项目id", required = true)
-                                                      @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<PersonalFilterVO> queryById(@ApiParam(value = "组织id", required = true)
+                                                      @PathVariable(name = "organization_id") Long organizationId,
                                                       @ApiParam(value = "filter id", required = true)
                                                       @PathVariable @Encrypt Long filterId) {
-        return Optional.ofNullable(personalFilterService.queryById(0L, projectId, filterId))
+        return Optional.ofNullable(personalFilterService.queryById(organizationId, 0L, filterId))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.personalFilter.queryById"));
     }
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("我的筛选重名校验")
+    @ApiOperation("组织层我的筛选重名校验")
     @GetMapping(value = "/check_name")
-    public ResponseEntity<Boolean> checkName(@ApiParam(value = "项目id", required = true)
-                                             @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<Boolean> checkName(@ApiParam(value = "组织id", required = true)
+                                             @PathVariable(name = "organization_id") Long organizationId,
                                              @ApiParam(value = "用户id", required = true)
                                              @RequestParam @Encrypt Long userId,
                                              @ApiParam(value = "name", required = true)
                                              @RequestParam String name,
-                                             @ApiParam(value = "filterTypeCode", required = true)
+                                             @ApiParam(value = "类型code", required = true)
                                              @RequestParam String filterTypeCode) {
-        return Optional.ofNullable(personalFilterService.checkName(0L, projectId, userId, name, filterTypeCode))
+        return Optional.ofNullable(personalFilterService.checkName(organizationId, 0L, userId, name, filterTypeCode))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.checkName.get"));
     }
 
     @Permission(level = ResourceLevel.ORGANIZATION)
-    @ApiOperation("设置默认筛选")
+    @ApiOperation("组织层设置默认筛选")
     @PutMapping(value = "/set_default")
-    public ResponseEntity<Boolean> setDefault(@ApiParam(value = "项目id", required = true)
-                                             @PathVariable(name = "project_id") Long projectId,
+    public ResponseEntity<Boolean> setDefault(@ApiParam(value = "组织id", required = true)
+                                             @PathVariable(name = "organization_id") Long organizationId,
                                              @ApiParam(value = "filter Id", required = true)
                                              @RequestParam @Encrypt Long filterId) {
-        return Optional.ofNullable(personalFilterService.setDefault(0L, projectId, filterId))
+        return Optional.ofNullable(personalFilterService.setDefault(organizationId, 0L, filterId))
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.personal.filter.set.default"));
     }
--- src/main/java/io/choerodon/agile/app/service/impl/PersonalFilterServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/PersonalFilterServiceImpl.java
@@ -3,6 +3,7 @@ package io.choerodon.agile.app.service.impl;
 import io.choerodon.agile.api.vo.PersonalFilterVO;
 import io.choerodon.agile.app.service.PersonalFilterService;
 import io.choerodon.agile.infra.dto.PersonalFilterDTO;
+import io.choerodon.agile.infra.enums.PersonalFilterTypeCode;
 import io.choerodon.agile.infra.mapper.PersonalFilterMapper;
 import io.choerodon.agile.infra.utils.EncryptionUtils;
 import io.choerodon.core.exception.CommonException;
@@ -14,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author shinan.chen
@@ -32,15 +35,20 @@ public class PersonalFilterServiceImpl implements PersonalFilterService {
     public static final String NAME_ERROR = "error.personalFilter.nameNotNull";
     public static final String INSERT_ERROR = "error.personalFilter.create";
     public static final String NAME_EXIST = "error.personalFilter.nameExist";
+    public static final String TYPE_CODE_ILLEGAL = "error.personalFilter.typeCode.illegal";
+    public static final String TYPE_CODE_NOT_NULL = "error.personalFilter.typeCode.notNull";
+    private static final String[] FILTER_TYPE_CODES = {PersonalFilterTypeCode.AGILE_ISSUE, PersonalFilterTypeCode.AGILE_WORK_HOURS};
+
     @Autowired
     private ModelMapper modelMapper;
 
 
     @Override
-    public PersonalFilterVO queryById(Long projectId, Long filterId) {
+    public PersonalFilterVO queryById(Long organizationId, Long projectId, Long filterId) {
         PersonalFilterDTO personalFilter = new PersonalFilterDTO();
         personalFilter.setFilterId(filterId);
         personalFilter.setProjectId(projectId);
+        personalFilter.setProjectId(organizationId);
         PersonalFilterDTO personalFilterDTO = personalFilterMapper.selectByPrimaryKey(personalFilter);
         if (personalFilterDTO == null) {
             throw new CommonException(NOTFOUND_ERROR);
@@ -50,47 +58,67 @@ public class PersonalFilterServiceImpl implements PersonalFilterService {
     }
 
     @Override
-    public PersonalFilterVO create(Long projectId, PersonalFilterVO personalFilterVO) {
+    public PersonalFilterVO create(Long organizationId, Long projectId, PersonalFilterVO personalFilterVO) {
+        String filterTypeCode = personalFilterVO.getFilterTypeCode();
+        checkFilterTypeCode(filterTypeCode);
         if (personalFilterVO.getName() == null || personalFilterVO.getName().equals("")) {
             throw new CommonException(NAME_ERROR);
         }
         CustomUserDetails customUserDetails = DetailsHelper.getUserDetails();
         Long userId = customUserDetails.getUserId();
-        if (Boolean.TRUE.equals(checkName(projectId, userId, personalFilterVO.getName()))) {
+        if (Boolean.TRUE.equals(checkName(organizationId, projectId, userId, personalFilterVO.getName(), filterTypeCode))) {
             throw new CommonException(NAME_EXIST);
         }
         if (!ObjectUtils.isEmpty(personalFilterVO.getDefault()) && Boolean.TRUE.equals(personalFilterVO.getDefault())) {
-            personalFilterMapper.updateDefault(projectId, userId, false, null);
+            personalFilterMapper.updateDefault(organizationId, projectId, userId, false, null, filterTypeCode);
         }
         personalFilterVO.setUserId(userId);
         personalFilterVO.setProjectId(projectId);
+        personalFilterVO.setOrganizationId(organizationId);
         personalFilterVO.setFilterJson(EncryptionUtils.handlerPersonFilterJson(personalFilterVO.getFilterJson(),false));
         PersonalFilterDTO personalFilterDTO = modelMapper.map(personalFilterVO, PersonalFilterDTO.class);
         if (personalFilterMapper.insert(personalFilterDTO) != 1) {
             throw new CommonException(INSERT_ERROR);
         }
-        return queryById(projectId, personalFilterDTO.getFilterId());
+        return queryById(organizationId, projectId, personalFilterDTO.getFilterId());
+    }
+
+    private void checkFilterTypeCode(String filterTypeCode) {
+        if (Objects.isNull(filterTypeCode) || Objects.equals("", filterTypeCode)) {
+            throw new CommonException(TYPE_CODE_NOT_NULL);
+        }
+        if (!Arrays.asList(FILTER_TYPE_CODES).contains(filterTypeCode)) {
+            throw new CommonException(TYPE_CODE_ILLEGAL);
+        }
     }
 
     @Override
-    public PersonalFilterVO update(Long projectId, Long filterId, PersonalFilterVO personalFilterVO) {
+    public PersonalFilterVO update(Long organizationId, Long projectId, Long filterId, PersonalFilterVO personalFilterVO) {
+        PersonalFilterDTO dto = personalFilterMapper.selectByPrimaryKey(filterId);
+        if (Objects.isNull(dto)) {
+            throw new CommonException(NOTFOUND_ERROR);
+        }
+        Long userId = DetailsHelper.getUserDetails().getUserId();
+        if (Boolean.TRUE.equals(checkName(organizationId, projectId, userId, personalFilterVO.getName(), dto.getFilterTypeCode()))) {
+            throw new CommonException(NAME_EXIST);
+        }
         personalFilterVO.setFilterId(filterId);
         PersonalFilterDTO personalFilterDTO = modelMapper.map(personalFilterVO, PersonalFilterDTO.class);
         personalFilterDTO.setFilterJson(EncryptionUtils.handlerPersonFilterJson(personalFilterDTO.getFilterJson(),false));
         if (!ObjectUtils.isEmpty(personalFilterVO.getDefault()) && Boolean.TRUE.equals(personalFilterVO.getDefault())) {
-            Long userId = DetailsHelper.getUserDetails().getUserId();
-            personalFilterMapper.updateDefault(projectId, userId, false, null);
+            personalFilterMapper.updateDefault(organizationId, projectId, userId, false, null, dto.getFilterTypeCode());
         }
         if (personalFilterMapper.updateByPrimaryKeySelective(personalFilterDTO) != 1) {
             throw new CommonException(UPDATE_ERROR);
         }
-        return queryById(projectId, filterId);
+        return queryById(organizationId, projectId, filterId);
     }
 
     @Override
-    public void deleteById(Long projectId, Long filterId) {
+    public void deleteById(Long organizationId, Long projectId, Long filterId) {
         PersonalFilterDTO personalFilterDTO = new PersonalFilterDTO();
         personalFilterDTO.setProjectId(projectId);
+        personalFilterDTO.setOrganizationId(organizationId);
         personalFilterDTO.setFilterId(filterId);
         int isDelete = personalFilterMapper.delete(personalFilterDTO);
         if (isDelete != 1) {
@@ -99,28 +127,34 @@ public class PersonalFilterServiceImpl implements PersonalFilterService {
     }
 
     @Override
-    public List<PersonalFilterVO> listByProjectId(Long projectId, Long userId, String searchStr) {
-        List<PersonalFilterVO> list = modelMapper.map(personalFilterMapper.queryByProjectIdAndUserId(projectId, userId, searchStr), new TypeToken<List<PersonalFilterVO>>() {
+    public List<PersonalFilterVO> listByUserId(Long organizationId, Long projectId, Long userId, String searchStr, String filterTypeCode) {
+        checkFilterTypeCode(filterTypeCode);
+        List<PersonalFilterVO> list = modelMapper.map(
+                personalFilterMapper.queryByProjectIdAndUserId(organizationId, projectId, userId, searchStr, filterTypeCode), new TypeToken<List<PersonalFilterVO>>() {
         }.getType());
         list.forEach(v -> v.setFilterJson(EncryptionUtils.handlerPersonFilterJson(v.getFilterJson(),true)));
         return list;
     }
 
     @Override
-    public Boolean checkName(Long projectId, Long userId, String name) {
+    public Boolean checkName(Long organizationId, Long projectId, Long userId, String name, String filterTypeCode) {
+        checkFilterTypeCode(filterTypeCode);
         PersonalFilterDTO personalFilterDTO = new PersonalFilterDTO();
         personalFilterDTO.setProjectId(projectId);
+        personalFilterDTO.setOrganizationId(organizationId);
         personalFilterDTO.setUserId(userId);
         personalFilterDTO.setName(name);
+        personalFilterDTO.setFilterTypeCode(filterTypeCode);
         List<PersonalFilterDTO> list = personalFilterMapper.select(personalFilterDTO);
         return list != null && !list.isEmpty();
     }
 
     @Override
-    public Boolean setDefault(Long projectId, Long filterId) {
+    public Boolean setDefault(Long organizationId, Long projectId, Long filterId) {
         Long userId = DetailsHelper.getUserDetails().getUserId();
-        personalFilterMapper.updateDefault(projectId, userId, false, null);
-        int result = personalFilterMapper.updateDefault(projectId, userId, true, filterId);
+        String filterTypeCode = queryById(organizationId, projectId, filterId).getFilterTypeCode();
+        personalFilterMapper.updateDefault(organizationId, projectId, userId, false, null, filterTypeCode);
+        int result = personalFilterMapper.updateDefault(organizationId, projectId, userId, true, filterId, filterTypeCode);
         return result > 0;
     }
 }
