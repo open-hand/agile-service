13b7f32b87a8b4cebb092527ad8ee2aaed972886
superlee <superleader8@gmail.com>
2021-11-17 14:13:34 +0800

    [ADD] 甘特图前置项树形结构测试验证接口

---
+++ src/main/java/io/choerodon/agile/api/vo/IssueTreeVO.java
@@ -0,0 +1,91 @@
+package io.choerodon.agile.api.vo;
+
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author superlee
+ * @since 2021-11-17
+ */
+public class IssueTreeVO {
+
+    private Long issueId;
+
+    private String summary;
+
+    private String issueNum;
+
+    private List<IssueTreeVO> children;
+
+    public Long getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(Long issueId) {
+        this.issueId = issueId;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getIssueNum() {
+        return issueNum;
+    }
+
+    public void setIssueNum(String issueNum) {
+        this.issueNum = issueNum;
+    }
+
+    public List<IssueTreeVO> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<IssueTreeVO> children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IssueTreeVO)) return false;
+        IssueTreeVO that = (IssueTreeVO) o;
+        return getIssueId().equals(that.getIssueId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIssueId());
+    }
+
+    public String toString(int level, StringBuilder builder) {
+        if (builder == null) {
+            builder = new StringBuilder();
+        }
+        String suojin = "  ";
+        for (int i = 0; i < level; i++) {
+            builder.append(suojin);
+        }
+        builder
+                .append(this.summary)
+                .append("#")
+                .append(this.issueNum)
+                .append("#")
+                .append(this.issueId)
+                .append("\n");
+        List<IssueTreeVO> children = this.getChildren();
+        if (!ObjectUtils.isEmpty(children)) {
+            level++;
+            for (IssueTreeVO child : children) {
+                child.toString(level, builder);
+            }
+        }
+        return builder.toString();
+    }
+}
