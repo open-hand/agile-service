1680c93ca0d499fbf51f69b170239dfe7e1b65ef
superlee <superleader8@gmail.com>
2021-11-22 13:57:30 +0800

    [IMP] 组织层甘特图查询接口修改接口权限

--- src/main/java/io/choerodon/agile/api/controller/v1/OrganizationGanttChartController.java
+++ src/main/java/io/choerodon/agile/api/controller/v1/OrganizationGanttChartController.java
@@ -47,7 +47,7 @@ public class OrganizationGanttChartController {
     }
 
 
-    @Permission(level = ResourceLevel.ORGANIZATION, permissionLogin = true)
+    @Permission(level = ResourceLevel.ORGANIZATION)
     @ApiOperation("查询组织层项目列表数据")
     @GetMapping(value = "/agile_projects")
     public ResponseEntity<List<ProjectVO>> listAgileProjects(@ApiParam(value = "组织id", required = true)
