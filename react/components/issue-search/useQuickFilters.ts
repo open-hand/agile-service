33da7daf5f3d33ea173a6f8bf1c40bd6bcb8b34c
DingZhicheng <zhicheng.ding@hand-china.com>
2021-11-22 17:20:58 +0800

    [FIX]修复工作台模式筛选调用项目层快速筛选

--- react/components/issue-search/useQuickFilters.ts
+++ react/components/issue-search/useQuickFilters.ts
@@ -1,10 +1,18 @@
 import { useState, useEffect, useCallback } from 'react';
+import { UseQueryOptions } from 'react-query';
 import { quickFilterApi } from '@/api';
-import { IQuickFilter } from '@/components/quick-search';
 
-export default function useQuickFilters({ projectId }: { projectId?: string }) {
+import { IQuickFilter } from '@/components/quick-search';
+/**
+ * 获取快速筛选数据hook
+ * @param param0
+ * @param options
+ * @returns
+ */
+export default function useQuickFilters({ projectId }: { projectId?: string }, options: Pick<UseQueryOptions<any>, 'enabled'> = { enabled: true }) {
   const [data, setData] = useState<IQuickFilter[]>([]);
   const [loading, setLoading] = useState(false);
+
   const refresh = useCallback(async () => {
     setLoading(true);
     const res = await quickFilterApi.project(projectId).loadAll();
@@ -12,8 +20,8 @@ export default function useQuickFilters({ projectId }: { projectId?: string }) {
     setLoading(false);
   }, [projectId]);
   useEffect(() => {
-    refresh();
-  }, [refresh]);
+    options?.enabled && refresh();
+  }, [options?.enabled, refresh]);
   return {
     data,
     loading,
