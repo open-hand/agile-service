92d4bbab1cce8655eda3bafa703f5c11657bbc5b
20399 <704565704@qq.com>
2022-02-24 17:01:14 +0800

    [IMP] 创建里程碑时交付物数据处理

--- react/components/create-issue/hooks/dealWithWaterfallIssueVO.ts
+++ react/components/create-issue/hooks/dealWithWaterfallIssueVO.ts
@@ -3,19 +3,18 @@ import { IssueValueHook } from '../interface';
 import { WATERFALL_TYPE_CODES } from '@/constants/TYPE_CODE';
 
 const dealWithWaterfallIssueVO: IssueValueHook = (values) => {
-  if (values.parentId) {
-    if (WATERFALL_TYPE_CODES.includes(values.typeCode)) {
-      return {
-        ...omit(values, ['parentId']),
-        waterfallIssueVO: {
-          parentId: values.parentId,
-        },
-      };
-    }
+  if (WATERFALL_TYPE_CODES.includes(values.typeCode)) {
     return {
-      ...omit(values, ['parentId']),
+      ...omit(values, ['parentId', 'deliverableData']),
+      waterfallIssueVO: {
+        parentId: values.parentId || undefined,
+        wfDeliverableVOS: values.typeCode === 'milestone' ? values.deliverableData : undefined,
+      },
     };
   }
+  return {
+    ...omit(values, ['parentId', 'deliverableData']),
+  };
   return values;
 };
 export default dealWithWaterfallIssueVO;
