8ae61e2f0a18b7e147f3cace818e8f5909d5d5af
lihao <310103270@qq.com>
2022-04-17 14:30:14 +0800

    [IMP] 修改列配置接口访问权限

--- src/main/java/io/choerodon/agile/api/controller/v1/OrganizationListLayoutController.java
+++ src/main/java/io/choerodon/agile/api/controller/v1/OrganizationListLayoutController.java
@@ -1,15 +1,16 @@
 package io.choerodon.agile.api.controller.v1;
 
-import io.choerodon.agile.api.vo.ListLayoutVO;
-import io.choerodon.agile.app.service.OrganizationListLayoutService;
-import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.swagger.annotation.Permission;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import io.choerodon.agile.api.vo.ListLayoutVO;
+import io.choerodon.agile.app.service.OrganizationListLayoutService;
+import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.swagger.annotation.Permission;
 
 
 /**
@@ -23,7 +24,7 @@ public class OrganizationListLayoutController {
     @Autowired
     private OrganizationListLayoutService organizationListLayoutService;
 
-    @Permission(level = ResourceLevel.ORGANIZATION)
+    @Permission(level = ResourceLevel.ORGANIZATION, permissionLogin = true)
     @ApiOperation("根据列表编码查询展示列配置")
     @GetMapping("/{apply_type}")
     public ResponseEntity<ListLayoutVO> queryByApplyType(@ApiParam(value = "组织id", required = true)
@@ -33,7 +34,7 @@ public class OrganizationListLayoutController {
         return new ResponseEntity<>(organizationListLayoutService.queryByApplyType(organizationId, applyType), HttpStatus.OK);
     }
 
-    @Permission(level = ResourceLevel.ORGANIZATION)
+    @Permission(level = ResourceLevel.ORGANIZATION, permissionLogin = true)
     @ApiOperation("保存列表布局配置")
     @PostMapping
     public ResponseEntity<ListLayoutVO> save(@ApiParam(value = "组织id", required = true)
