fded1a8e6462154e4b3b54fe831b6579cd76944f
huaxin <2502353032@qq.com>
2022-05-06 18:25:54 +0800

    [ADD] 产品增加标签字段

---
+++ src/main/java/io/choerodon/agile/api/vo/business/ProductLabelVO.java
@@ -0,0 +1,45 @@
+package io.choerodon.agile.api.vo.business;
+
+import io.swagger.annotations.ApiModelProperty;
+import org.hzero.starter.keyencrypt.core.Encrypt;
+
+/**
+ * @author huaxin.deng@hand-china.com
+ * @since 2022/5/6
+ */
+public class ProductLabelVO {
+
+    @ApiModelProperty(value = "标签id")
+    @Encrypt
+    private Long id;
+
+    @ApiModelProperty(value = "标签名称")
+    private String labelName;
+
+    @ApiModelProperty(value = "组织id")
+    private Long organizationId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabelName() {
+        return labelName;
+    }
+
+    public void setLabelName(String labelName) {
+        this.labelName = labelName;
+    }
+
+    public Long getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Long organizationId) {
+        this.organizationId = organizationId;
+    }
+}
