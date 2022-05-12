42bb466b53a0e7931e8cb1aa7a6afd2df06fcc0f
wangxiang <xiangwang04@hand-china.com>
2022-05-12 11:36:10 +0800

    [IMP] 增加两个属性

--- src/main/java/io/choerodon/agile/api/vo/WorkSpaceVO.java
+++ src/main/java/io/choerodon/agile/api/vo/WorkSpaceVO.java
@@ -1,5 +1,6 @@
 package io.choerodon.agile.api.vo;
 
+import io.swagger.annotations.ApiModelProperty;
 import org.hzero.starter.keyencrypt.core.Encrypt;
 
 public class WorkSpaceVO {
@@ -9,6 +10,11 @@ public class WorkSpaceVO {
     private String name;
     @Encrypt
     private Long baseId;
+
+    @ApiModelProperty("WorkSpace的类型")
+    private String type;
+    @ApiModelProperty("文件的类型")
+    private String fileType;
 
     public Long getId() {
         return id;
@@ -32,5 +38,21 @@ public class WorkSpaceVO {
 
     public void setBaseId(Long baseId) {
         this.baseId = baseId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
     }
 }
