f5287e1e288f88e40111e5e2183d6f1f42c39d50
DingZhicheng <ding25410@foxmail.com>
2022-05-31 17:08:05 +0800

    [IMP] 调整useFollowMouseTooltip位置更新方法

--- react/hooks/useFollowMouseTooltip.tsx
+++ react/hooks/useFollowMouseTooltip.tsx
@@ -1,10 +1,41 @@
-import React, { useCallback, useRef } from 'react';
-import { Tooltip } from 'choerodon-ui/pro';
-import { useCreation, usePersistFn } from 'ahooks';
-import { isEqual } from 'lodash';
+import React, {
+  createRef,
+  RefObject,
+} from 'react';
+import { pick } from 'lodash';
+import { useCreation, useDebounceFn, usePersistFn } from 'ahooks';
+import { render } from 'react-dom';
+import useTooltip from './useTooltip';
+import '../style/follow-mouse.less';
 
 interface IHookFollowMouseTooltipProps {
-    tooltipTitle?: React.ReactNode
+  tooltipTitle?: React.ReactNode
+}
+let currentDom: any;
+function getRoot() {
+  const dom = document.body.querySelector('div.c7n-agile-follow-mouse-root');
+  if (dom) {
+    return dom;
+  }
+  const root = document.createElement('div');
+  root.className = 'c7n-agile-follow-mouse-root';
+  document.body.appendChild(root);
+  return root;
+}
+async function getMouseContainer(): Promise<RefObject<HTMLDivElement>> {
+  if (currentDom) {
+    return currentDom;
+  }
+  return new Promise((resolve) => {
+    const root = getRoot();
+    if (root) {
+      const ref = createRef<HTMLDivElement>();
+      render(<div ref={ref} className="c7n-agile-follow-mouse-dom" />, root, () => {
+        currentDom = ref;
+        resolve(ref);
+      });
+    }
+  });
 }
 /**
  * 跟随鼠标移动的tooltip
@@ -12,30 +43,31 @@ interface IHookFollowMouseTooltipProps {
  * @returns
  */
 function useFollowMouseTooltip({ tooltipTitle }: IHookFollowMouseTooltipProps) {
-  const tooltipPropsRef = useRef<any>();
-  const targetRef = useRef<any>();
-  const handleTooltipMouseLeave = useCallback(() => Tooltip.hide(), []);
-  const handleTooltipMouseEnter = useCallback((e) => {
-    targetRef.current = e.target;
-    Tooltip.show(e.target, {
-      onPopupAlign: (source: HTMLDivElement, align: object, target: Node | Window, translate: { x: number; y: number }) => {
-        const left = Math.max(0, (e.clientX || 0) - 21);
-        left && source.style.setProperty('left', `${left}px`);
-      },
-      autoAdjustOverflow: true,
-      arrowPointAtCenter: true,
-      title: tooltipTitle,
-      placement: 'topLeft',
-      ...tooltipPropsRef.current,
-    });
-  }, [tooltipTitle]);
-  const handleUpdateTooltip = usePersistFn((tooltipProps: { title?: any, [key: string]: any }) => {
-    if (!isEqual(tooltipProps, tooltipPropsRef.current)) {
-      tooltipPropsRef.current = tooltipProps;
-      targetRef.current && handleTooltipMouseEnter(targetRef.current);
+  const { onMouseLeave, onMouseEnter, updateTooltipProps } = useTooltip({ tooltip: { title: tooltipTitle } });
+  const { run: showTooltip, cancel } = useDebounceFn(async ({ pageX, pageY }: any) => {
+    const targetRef = await getMouseContainer();
+    if (targetRef.current) {
+      targetRef.current.setAttribute('style', `left:${pageX}px;top:${pageY}px;`);
     }
+    onMouseEnter({ target: targetRef.current }, { arrowPointAtCenter: true });
+  }, { wait: 600 });
+
+  const handleTooltipMouseEnter = usePersistFn((event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    showTooltip(pick(event, ['pageX', 'pageY']));
   });
-  const actions = useCreation(() => ({ update: handleUpdateTooltip }), []);
-  return [{ actions }, { onMouseMove: handleTooltipMouseEnter, onMouseEnter: handleTooltipMouseEnter, onMouseLeave: handleTooltipMouseLeave }] as const;
+
+  const actions = useCreation(() => ({ update: updateTooltipProps }), []);
+  return [{ actions }, {
+    onMouseMove: handleTooltipMouseEnter,
+    onMouseEnter: handleTooltipMouseEnter,
+    onMouseLeave: (event: React.MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+      cancel();
+      onMouseLeave();
+    },
+  }] as const;
 }
 export default useFollowMouseTooltip;
