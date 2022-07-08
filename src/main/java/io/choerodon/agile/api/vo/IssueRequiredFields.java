6405efa240755f36a3f45773599b72018ac16874
戴高阔 <gaokuo.dai@zknow.com>
2022-07-08 19:57:10 +0800

    [FIX]修复复制瀑布工作项超过2层时补全必输字段界面概述丢失的问题

--- src/main/java/io/choerodon/agile/api/vo/IssueRequiredFields.java
+++ src/main/java/io/choerodon/agile/api/vo/IssueRequiredFields.java
@@ -1,9 +1,10 @@
 package io.choerodon.agile.api.vo;
 
-import io.swagger.annotations.ApiModelProperty;
-import org.hzero.starter.keyencrypt.core.Encrypt;
-
 import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import org.hzero.starter.keyencrypt.core.Encrypt;
 
 /**
  * @author huaxin.deng@hand-china.com
@@ -15,22 +16,46 @@ public class IssueRequiredFields {
     @ApiModelProperty(value = "issue id")
     private Long issueId;
 
+    @ApiModelProperty(value = "问题概要")
+    private String summary;
+
     @ApiModelProperty(value = "必填字段")
     private List<PageFieldViewVO> requiredFields;
 
+    /**
+     * @return issue id
+     */
     public Long getIssueId() {
         return issueId;
     }
 
-    public void setIssueId(Long issueId) {
+    public IssueRequiredFields setIssueId(Long issueId) {
         this.issueId = issueId;
+        return this;
     }
 
+    /**
+     * @return 问题概要
+     */
+    public String getSummary() {
+        return this.summary;
+    }
+
+    public IssueRequiredFields setSummary(String summary) {
+        this.summary = summary;
+        return this;
+    }
+
+    /**
+     * @return 必填字段
+     */
     public List<PageFieldViewVO> getRequiredFields() {
         return requiredFields;
     }
 
-    public void setRequiredFields(List<PageFieldViewVO> requiredFields) {
+    public IssueRequiredFields setRequiredFields(List<PageFieldViewVO> requiredFields) {
         this.requiredFields = requiredFields;
+        return this;
     }
+
 }
