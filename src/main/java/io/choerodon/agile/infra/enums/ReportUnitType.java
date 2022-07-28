be6921b7a05cf2273517ad42e7ab182012534b1e
李楷文 <kaiwen.li@zknow.com>
2022-07-28 11:07:33 +0800

    [ADD] 项目报告创建支持风险类型

---
+++ src/main/java/io/choerodon/agile/infra/enums/ReportUnitType.java
@@ -0,0 +1,34 @@
+package io.choerodon.agile.infra.enums;
+
+/**
+ * @author superlee
+ * @since 2022-07-27
+ */
+public class ReportUnitType {
+
+    /**
+     * 文本
+     */
+    public static final String TEXT = "text";
+    /**
+     * 图表
+     */
+    public static final String CHART = "chart";
+    /**
+     * 工作项静态列表
+     */
+    public static final String STATIC_LIST = "static_list";
+    /**
+     * 风险静态列表
+     */
+    public static final String STATIC_LIST_RISK = "static_list_risk";
+    /**
+     * 工作项动态列表
+     */
+    public static final String DYNAMIC_LIST = "dynamic_list";
+    /**
+     * 风险动态列表
+     */
+    public static final String DYNAMIC_LIST_RISK = "dynamic_list_risk";
+
+}
