be0ad2e50a6c779d1330528765ced12f85a93766
戴高阔 <gaokuo.dai@zknow.com>
2022-09-09 11:53:56 +0800

    [FIX]个人筛选数据修复程序数组越界问题

--- src/main/java/io/choerodon/agile/infra/upgrade/UpgradeDataFixService.java
+++ src/main/java/io/choerodon/agile/infra/upgrade/UpgradeDataFixService.java
@@ -3,21 +3,23 @@ package io.choerodon.agile.infra.upgrade;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import io.choerodon.agile.api.vo.ProjectVO;
-import io.choerodon.agile.infra.dto.PersonalFilterDTO;
-import io.choerodon.agile.infra.enums.ProjectCategory;
-import io.choerodon.agile.infra.feign.operator.RemoteIamOperator;
-import io.choerodon.agile.infra.feign.vo.ProjectCategoryDTO;
-import io.choerodon.agile.infra.mapper.PersonalFilterMapper;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.hzero.core.base.BaseConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import io.choerodon.agile.api.vo.ProjectVO;
+import io.choerodon.agile.infra.dto.PersonalFilterDTO;
+import io.choerodon.agile.infra.enums.ProjectCategory;
+import io.choerodon.agile.infra.feign.operator.RemoteIamOperator;
+import io.choerodon.agile.infra.feign.vo.ProjectCategoryDTO;
+import io.choerodon.agile.infra.mapper.PersonalFilterMapper;
+
+import org.hzero.core.base.BaseConstants;
 
 /**
  * 升级数据修复专用Service
@@ -102,8 +104,8 @@ public class UpgradeDataFixService {
                             "\t\t\t)) temp \n" +
                             "\t)",
                     (ps -> {
-                        for (int i = 1; i <= projectIds.size(); i++) {
-                            ps.setLong(i, projectIds.get(i));
+                        for (int i = 0; i < projectIds.size(); i++) {
+                            ps.setLong(i+1, projectIds.get(i));
                         }
                     })
             );
@@ -141,8 +143,8 @@ public class UpgradeDataFixService {
                             "\t\t\t)) temp \n" +
                             "\t)",
                     (ps -> {
-                        for (int i = 1; i <= projectIds.size(); i++) {
-                            ps.setLong(i, projectIds.get(i));
+                        for (int i = 0; i < projectIds.size(); i++) {
+                            ps.setLong(i+1, projectIds.get(i));
                         }
                     })
             );
