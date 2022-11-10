dd674eddd227a31bc972d51fd746fbeaa0e7e75d
李楷文 <kaiwen.li@zknow.com>
2022-11-10 18:26:18 +0800

    [FIX] 修复高级筛选系统下拉框为空时可能报控制针异常的问题

--- src/main/java/io/choerodon/agile/api/vo/search/Value.java
+++ src/main/java/io/choerodon/agile/api/vo/search/Value.java
@@ -29,26 +29,22 @@ public class Value {
     @ApiModelProperty("未加密id集合")
     private List<Long> noEncryptIdList;
 
-    @ApiModelProperty("数字快速搜索值")
-    @Deprecated
-    private Integer valueInteger;
-    @ApiModelProperty("数字数组快速搜索值")
-    @Deprecated
-    private List<Integer> valueIntegerList;
     @ApiModelProperty("数字(允许小数)快速搜索值")
     private BigDecimal valueDecimal;
-    @ApiModelProperty("数字(允许小数)数组快速搜索值")
-    private List<BigDecimal> valueDecimalList;
-    @ApiModelProperty("bool快速搜索值")
-    @Deprecated
-    private Boolean valueBool;
     @ApiModelProperty("时间快速搜索值")
     private Date valueDate;
-    @ApiModelProperty("时分秒快速搜索值")
-    @Deprecated
-    private String valueDateTime;
 
     private String valueSpecial;
+
+    private List<Object> objectList;
+
+    public List<Object> getObjectList() {
+        return objectList;
+    }
+
+    public void setObjectList(List<Object> objectList) {
+        this.objectList = objectList;
+    }
 
     public String getValueStr() {
         return valueStr;
@@ -98,22 +94,6 @@ public class Value {
         this.noEncryptIdList = noEncryptIdList;
     }
 
-    public Integer getValueInteger() {
-        return valueInteger;
-    }
-
-    public void setValueInteger(Integer valueInteger) {
-        this.valueInteger = valueInteger;
-    }
-
-    public List<Integer> getValueIntegerList() {
-        return valueIntegerList;
-    }
-
-    public void setValueIntegerList(List<Integer> valueIntegerList) {
-        this.valueIntegerList = valueIntegerList;
-    }
-
     public BigDecimal getValueDecimal() {
         return valueDecimal;
     }
@@ -122,36 +102,12 @@ public class Value {
         this.valueDecimal = valueDecimal;
     }
 
-    public List<BigDecimal> getValueDecimalList() {
-        return valueDecimalList;
-    }
-
-    public void setValueDecimalList(List<BigDecimal> valueDecimalList) {
-        this.valueDecimalList = valueDecimalList;
-    }
-
-    public Boolean getValueBool() {
-        return valueBool;
-    }
-
-    public void setValueBool(Boolean valueBool) {
-        this.valueBool = valueBool;
-    }
-
     public Date getValueDate() {
         return valueDate;
     }
 
     public void setValueDate(Date valueDate) {
         this.valueDate = valueDate;
-    }
-
-    public String getValueDateTime() {
-        return valueDateTime;
-    }
-
-    public void setValueDateTime(String valueDateTime) {
-        this.valueDateTime = valueDateTime;
     }
 
     public String getValueSpecial() {
