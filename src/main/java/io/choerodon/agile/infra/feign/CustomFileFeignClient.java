7c955d0e33fd5a5257e1073059a11fd225aca396
汪翔 <xiang.wang@zknow.com>
2023-05-15 17:54:29 +0800

    [FIX] 调整代码

--- src/main/java/io/choerodon/agile/infra/feign/CustomFileFeignClient.java
+++ src/main/java/io/choerodon/agile/infra/feign/CustomFileFeignClient.java
@@ -3,14 +3,14 @@ package io.choerodon.agile.infra.feign;
 import java.util.List;
 import java.util.Map;
 
+import io.choerodon.agile.infra.feign.fallback.CustomFileRemoteFallbackFactory;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import io.choerodon.agile.infra.feign.fallback.CustomFileRemoteFallbackFactory;
 
 import org.hzero.common.HZeroService;
 
@@ -28,4 +28,9 @@ public interface CustomFileFeignClient {
 
     @PostMapping(value = {"/v1/{organizationId}/upload/fragment-combine"})
     ResponseEntity<String> fragmentCombineBlock(@PathVariable Long organizationId, @RequestParam String guid, @RequestParam String fileName, @RequestBody(required = false) Map<String, String> args);
+
+    @ApiOperation(value = "根据文件的keys集合 查询文件数据")
+    @PostMapping("/choerodon/v1/{organization_id}/file/list")
+    ResponseEntity<String> queryFileDTOByIds(@PathVariable("organization_id") Long organizationId,
+                                             @RequestBody List<String> fileKeys);
 }
--- src/main/java/io/choerodon/agile/infra/utils/FileCommonUtil.java
+++ src/main/java/io/choerodon/agile/infra/utils/FileCommonUtil.java
@@ -13,28 +13,6 @@ import java.net.URLEncoder;
  */
 public class FileCommonUtil {
 
-    public static String utf8Code(String str) {
-        try {
-            return URLEncoder.encode(str, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getFileId(String fileKey) {
-        if (StringUtils.isEmpty(fileKey)) {
-            return "";
-        }
-        int index = fileKey.indexOf("@");
-        if (index > -1) {
-            return fileKey.substring(fileKey.lastIndexOf("/") + 1, fileKey.indexOf("@"));
-        } else {
-            String[] s = fileKey.split("/");
-            return s[s.length - 2];
-        }
-    }
-
     public static String getFileType(String fileKey) {
         if (StringUtils.isEmpty(fileKey)) {
             return "";
@@ -47,49 +25,4 @@ public class FileCommonUtil {
         }
     }
 
-    public static String decoderFileKey(String fileKey) {
-        //测试wps新版本是否需要base64
-        try {
-            String encode = URLEncoder.encode(fileKey, "UTF-8");
-            return URLDecoder.decode(encode, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getFileName(String fileKey) {
-        if (StringUtils.isEmpty(fileKey)) {
-            return "";
-        }
-        int index = fileKey.indexOf("@");
-        if (index > -1) {
-            return fileKey.substring(index + 1);
-        } else {
-            String[] s = fileKey.split("/");
-            return s[s.length - 1];
-        }
-    }
-
-
-    public static String getFileNameWithoutSuffix(String fileName) {
-        int index = fileName.indexOf(".");
-        if (index > -1) {
-            return fileName.substring(0, index);
-        } else {
-            return fileName;
-        }
-    }
-
-    public static String getFileTypeByFileName(String fileName) {
-        if (StringUtils.isEmpty(fileName)) {
-            return "";
-        }
-        int index = fileName.lastIndexOf(".");
-        if (index > -1) {
-            return fileName.substring(index + 1);
-        } else {
-            return "";
-        }
-    }
 }
